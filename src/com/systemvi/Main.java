c85eb79f414cc8876f1309ff87369096af19ae6e
Uros <karaleicuu@gmail.com>
2023-10-22 19:55:35 +0200

    kao radi fluid sim

--- src/com/systemvi/Main.java
+++ src/com/systemvi/Main.java
@@ -1,13 +1,9 @@
 package com.systemvi;
 
-import com.systemvi.examples.applicationtest.TextureRendererTest;
-import com.systemvi.examples.flappybird.FlappyBird;
 import com.systemvi.examples.fluid.Fluid;
-import com.systemvi.examples.physics2d.Physics;
 
 public class Main {
     public static void main(String[] args) {
-
-        new Physics(3,3,60).run();
+        new Fluid(3,3,60).run();
     }
 }
--- src/com/systemvi/examples/fluid/Fluid.java
+++ src/com/systemvi/examples/fluid/Fluid.java
@@ -1,6 +1,8 @@
 package com.systemvi.examples.fluid;
 
 import com.systemvi.engine.application.Application;
+import com.systemvi.engine.camera.Camera;
+import com.systemvi.engine.renderers.ShapeRenderer;
 import com.systemvi.engine.window.Window;
 import static org.lwjgl.opengl.GL33.*;
 
@@ -11,10 +13,46 @@ public class Fluid extends Application {
     }
 
     Window window;
+    Simulation simulation;
+    ShapeRenderer renderer;
+    Camera camera;
+    float px,py,prevx,prevy;
+    boolean mouseDown;
+    int size;
 
     @Override
     public void setup() {
-        window=new Window(500,500,"Fluid Sim");
+        int width=800;
+        int height=800;
+        window=new Window(width,height,"Fluid Sim");
+        renderer=new ShapeRenderer();
+        camera=new Camera();
+        camera.setScale(1,-1,1);
+        camera.setScreenSize(width,height);
+        camera.setPosition(width/2,height/2,0);
+        camera.update();
+        renderer.setCamera(camera);
+        size=4;
+        System.out.println(width/size);
+        simulation=new Simulation(width/size,height/size);
+
+        window.addOnMouseMoveListener((x, y) -> {
+            prevx=px;
+            prevy=py;
+            px= (float) x;
+            py= (float) y;
+        });
+        window.addOnMouseUpListener((button, mods) -> {
+            mouseDown=false;
+        });
+        window.addOnMouseDownListener((button, mods) -> {
+            mouseDown=true;
+        });
+        window.addOnResizeListener((width1, height1) -> {
+            camera.setPosition(width1/2,height1/2,0);
+            camera.setScreenSize(width1,height1);
+            camera.update();
+        });
     }
 
     @Override
@@ -24,6 +62,17 @@ public class Fluid extends Application {
 
         glClearColor(0,0,0,1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        if(mouseDown){
+            simulation.add((int) (px/size), (int) (py/size),size);
+            simulation.addVelocity((int) (px/size), (int) (py/size),px-prevx,py-prevy,size);
+        }
+
+        simulation.dens_step(0.000f,delta);
+        simulation.vel_step(0.0f,delta);
+
+        simulation.draw(renderer,size);
+        renderer.flush();
 
         window.swapBuffers();
     }
