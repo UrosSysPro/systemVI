5847aa1249bc33fa584a3740efa98607071bf98c
Uros <karaleicuu@gmail.com>
2023-12-23 19:59:29 +0100

    tetris

---
+++ src/com/systemvi/examples/tetris/Block.java
@@ -0,0 +1,24 @@
+package com.systemvi.examples.tetris;
+
+import com.googlecode.lanterna.TextColor;
+
+public class Block {
+    private final TextColor[][][] data;
+    private final TextColor color;
+    private final int currentRotation;
+
+    public Block(TextColor[][][] data,TextColor color){
+        this.color=color;
+        currentRotation=0;
+        this.data=data;
+    }
+    public int getWidth(){
+        return data[currentRotation].length;
+    }
+    public int getHeight(){
+        return data[currentRotation][0].length;
+    }
+    public TextColor get(int x,int y){
+        return data[currentRotation][x][y];
+    }
+}
