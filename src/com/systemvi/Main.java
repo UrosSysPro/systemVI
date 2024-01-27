1048294416bbed47ab3dcf5f8403e0f4ad746bc1
uros <karaleicu@gmail.com>
2024-01-27 19:56:00 +0100

    merge zbog gluposti sa mandelbrot set sa casa

--- src/com/systemvi/Main.java
+++ src/com/systemvi/Main.java
@@ -1,14 +1,11 @@
 package com.systemvi;
 
 import com.systemvi.examples.fractals.Mandelbrotset;
-import com.systemvi.examples.minecraft.DebugApp;
-import com.systemvi.examples.texturetest.Paint;
+import com.systemvi.examples.compute.GameOfLife;
 
 public class Main {
     public static void main(String[] args){
-//        new DebugApp(3,3,60).run();
 //        new GameOfLife(4,6,60).run();
-//        new Paint(3,3,60,800,600,"Paint").run();
         new Mandelbrotset(3,3,60,800,600,"Mandelbrot Set").run();
     }
 }
---
+++ src/com/systemvi/examples/compute/Graph.java
@@ -0,0 +1,20 @@
+package com.systemvi.examples.compute;
+
+import com.systemvi.engine.application.Game;
+import com.systemvi.engine.window.Window;
+
+public class Graph extends Game {
+    public Graph(int openglVersionMajor, int openglVersionMinor, int targetFPS, int windowWidth, int windowHeight, String title) {
+        super(openglVersionMajor, openglVersionMinor, targetFPS, windowWidth, windowHeight, title);
+    }
+
+    @Override
+    public void setup(Window window) {
+
+    }
+
+    @Override
+    public void loop(float delta) {
+
+    }
+}
