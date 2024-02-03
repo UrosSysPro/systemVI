3cb396511842eed545ad55c367af6f4efc9358de
uros <karaleicu@gmail.com>
2024-02-03 01:52:02 +0100

    pocetak za model loader

---
+++ src/com/systemvi/examples/minecraft/world/ChunkCache.java
@@ -0,0 +1,15 @@
+package com.systemvi.examples.minecraft.world;
+
+import org.joml.Vector3i;
+
+public class ChunkCache {
+    private Chunk chunk;
+
+
+    public void generate(){
+
+    }
+    public void update(Vector3i position){
+
+    }
+}
