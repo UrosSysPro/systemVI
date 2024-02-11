02caa98eb31f849b19d3702530e547f3a03df16e
Uros <karaleicuu@gmail.com>
2024-02-11 18:32:29 +0100

    column widget

---
+++ src/com/systemvi/engine/ui/widgets/Column.scala
@@ -0,0 +1,69 @@
+package com.systemvi.engine.ui.widgets
+
+import com.systemvi.engine.ui.{Widget, WidgetRenderer}
+import org.joml.Vector2f
+
+class Column(val children:Array[Widget]) extends StatelessWidget {
+  override def build(): Widget = null
+  override def calculateSize(maxParentSize: Vector2f): Vector2f = {
+    if(children==null){
+      size.set(maxParentSize)
+      return size
+    }
+    var totalFlex:Float=0
+    var totalHeight:Float=0
+    var nonFlexSize:Float=0
+    var maxWidth:Float=0
+    //izracunati velicinu za elemente koji nisu expanded
+    for(child<-children){
+      child match{
+        case expanded: Expanded => totalFlex += expanded.flex
+        case widget: Widget =>
+          val childSize=widget.calculateSize(maxParentSize)
+          nonFlexSize+=childSize.y
+          totalHeight+=childSize.y
+          if(childSize.x>maxWidth)maxWidth=childSize.x
+        case _=>
+      }
+    }
+    //izracunati velicinu za flex elemente
+    val availableHeight=Math.max(0,maxParentSize.y-nonFlexSize)
+    for(child<-children){
+      child match{
+        case expanded:Expanded=>
+          val childSize=expanded.calculateSize(new Vector2f(maxParentSize.y,availableHeight*expanded.flex/totalFlex))
+          if(childSize.x>maxWidth)maxWidth=childSize.x
+          totalHeight+=childSize.y
+        case _=>
+      }
+    }
+    size.set(maxWidth,totalHeight)
+  }
+  override def calculatePosition(parentPosition: Vector2f): Unit = {
+    position.set(parentPosition)
+    if(children==null)return
+    val currentPosition=new Vector2f(parentPosition)
+    for(child<-children){
+      child.calculatePosition(currentPosition)
+      currentPosition.y+=child.size.y
+    }
+  }
+  override def draw(renderer: WidgetRenderer): Unit = {
+    if(children==null)return
+    for(child<-children){
+      child.draw(renderer)
+    }
+  }
+  override def debugPrint(tabs: String): Unit = {
+    println(s"$tabs Column size: ${size.x} ${size.y} position: ${position.x} ${position.y}")
+    if(children==null)return
+    val childTabs=s"$tabs\t"
+    for(child<-children){
+      child.debugPrint(childTabs)
+    }
+  }
+}
+
+object Column{
+  def apply(children: Array[Widget]=null): Column = new Column(children)
+}
--- src/com/systemvi/examples/uitest/WidgetTest.scala
+++ src/com/systemvi/examples/uitest/WidgetTest.scala
@@ -2,7 +2,7 @@ package com.systemvi.examples.uitest
 
 import com.systemvi.engine.application.Game
 import com.systemvi.engine.ui.{Widget, WidgetRenderer}
-import com.systemvi.engine.ui.widgets.{Container, EdgeInsets, Expanded, Padding, Row, SizedBox}
+import com.systemvi.engine.ui.widgets.{Column, Container, EdgeInsets, Expanded, Padding, Row, SizedBox}
 import com.systemvi.engine.utils.Utils
 import com.systemvi.engine.utils.Utils.Buffer
 import com.systemvi.engine.window.Window
@@ -17,21 +17,14 @@ class WidgetTest extends Game(3,3,60,800,600,"Widget test"){
       color=new Vector4f(0.3f,0.6f,0.9f,1.0f),
       child=Padding(
         padding=EdgeInsets.all(20),
-        child=Row(
-          children = Array(
-            SizedBox(
-              size = new Vector2f(100,Float.MaxValue),
-              child = Container(color = new Vector4f(1,0,0,1))
-            ),
-            Expanded(
-              child = Container(color = new Vector4f(0,1,0,1))
-            ),
-            SizedBox(
-              size = new Vector2f(100,Float.MaxValue),
-              child = Container(color = new Vector4f(1,0,0,1))
-            )
-          )
-        ))
+        child=Column(
+          children = (0 until 3).map(rowIndex=>Expanded(child=Row(
+            children = (0 until 3).map(columnIndex=>Expanded(
+              child=Container(color = new Vector4f(rowIndex/3f,columnIndex/3f,0.9f,1.0f))
+            )).toArray
+          ))).toArray
+        )
+      )
     )
     widget.calculateSize(new Vector2f(800,600))
     widget.calculatePosition(new Vector2f(0,0))
