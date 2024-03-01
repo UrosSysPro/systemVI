4bf8aed90fa4568a78dd5f755528dc0491b23034
uros <karaleicu@gmail.com>
2024-03-01 22:12:53 +0100

    nista

--- demo-examples/src/com/systemvi/examples/Main.java
+++ demo-examples/src/com/systemvi/examples/Main.java
@@ -1,15 +1,17 @@
 package com.systemvi.examples;
 
 import com.systemvi.engine.utils.Utils;
+import com.systemvi.examples.compute.GameOfLife;
 import com.systemvi.examples.compute.fluid.App;
+import com.systemvi.examples.fractals.Juliaset;
 import com.systemvi.examples.uitest.WidgetTest;
 
 public class Main {
     public static void main(String[] args){
         Utils.assetsFolder="../";
-        new WidgetTest().run();
+//        new WidgetTest().run();
 //        new Fluid(3,3,60).run();
-//        new Juliaset(3,3,60,800,600,"Julia set").run();
+        new Juliaset(3,3,60,800,600,"Julia set").run();
 //        new App(3,3,60).run();
 //        new com.systemvi.examples.test3d.normalmapping.App(3,3,60).run();
 //        new App(3,3,60).run();
