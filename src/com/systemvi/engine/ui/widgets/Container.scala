74e978fa17b7a232266312d9db69d92b5e3a8b45
uros <karaleicu@gmail.com>
2024-03-07 22:13:18 +0100

    box shadow container

--- src/com/systemvi/engine/ui/widgets/Container.scala
+++ src/com/systemvi/engine/ui/widgets/Container.scala
@@ -1,23 +1,39 @@
 package com.systemvi.engine.ui.widgets
 
 import com.systemvi.engine.ui.utils.context.{BuildContext, DrawContext}
+import com.systemvi.engine.ui.utils.data.BoxDecoration
 import com.systemvi.engine.ui.{Widget, WidgetRenderer}
 import org.joml.Vector4f
 
 
-class Container(child:Widget,val color:Vector4f) extends StatelessWidget{
-  override def build(context:BuildContext): Widget = {
-    return child
-  }
+class Container(child:Widget,val color:Vector4f,boxDecoration: BoxDecoration) extends StatelessWidget{
+  override def build(context:BuildContext): Widget = child
 
   override def draw(context:DrawContext): Unit = {
-    context.renderer.rect(position.x,position.y,size.x,size.y,color)
+    if(boxDecoration!=null){
+      boxDecoration match {
+        case BoxDecoration(color, borderRadius, _, boxShadow)=>
+          boxShadow.foreach{
+            boxShadow=>context.renderer.rect(
+              position.x+boxShadow.offset.x-boxShadow.size/2f,
+              position.y+boxShadow.offset.y-boxShadow.size/2,
+              size.x+boxShadow.size,
+              size.y+boxShadow.size,
+              boxShadow.color,
+              borderRadius,
+              boxShadow.blur
+            )
+          }
+          context.renderer.rect(position.x,position.y,size.x,size.y,color,borderRadius,1)
+      }
+    }else if(color!=null){
+      context.renderer.rect(position.x,position.y,size.x,size.y,color)
+    }
     super.draw(context)
-    //post processing
   }
 }
 
 object Container{
-  def apply(child: Widget=null,color:Vector4f=new Vector4f(0,0,0,0)): Container = new Container(child,color)
+  def apply(child: Widget=null,color:Vector4f=null,boxDecoration: BoxDecoration=null): Container = new Container(child,color,boxDecoration)
 }
 
