0947995dc6f852cc314c6d88dbc9b39269adb67a
Uros <karaleicuu@gmail.com>
2024-03-11 20:34:55 +0100

    pocetak row

--- src/com/systemvi/engine/ui/widgets/Row.scala
+++ src/com/systemvi/engine/ui/widgets/Row.scala
@@ -1,12 +1,19 @@
 package com.systemvi.engine.ui.widgets
 
 import com.systemvi.engine.ui.utils.context.{BuildContext, DrawContext}
+import com.systemvi.engine.ui.utils.data.CrossAxisAlignment.CrossAxisAlignment
+import com.systemvi.engine.ui.utils.data.MainAxisAlignment.MainAxisAlignment
+import com.systemvi.engine.ui.utils.data.{CrossAxisAlignment, MainAxisAlignment, MainAxisSize}
+import com.systemvi.engine.ui.utils.data.MainAxisSize.MainAxisSize
 import com.systemvi.engine.ui.{Widget, WidgetRenderer}
 import org.joml.Vector2f
 
-import scala.collection.mutable
-
-class Row(val children:Array[Widget]) extends StatelessWidget {
+class Row(
+           val children:Array[Widget],
+           val mainAxisAlignment: MainAxisAlignment,
+           val crossAxisAlignment: CrossAxisAlignment,
+           val mainAxisSize: MainAxisSize
+         ) extends StatelessWidget {
   override def build(context:BuildContext): Widget = {
     null
   }
@@ -47,13 +54,15 @@ class Row(val children:Array[Widget]) extends StatelessWidget {
   override def calculatePosition(parentPosition: Vector2f): Unit = {
     position.set(parentPosition)
     if(children==null)return
-    val currentPosition=new Vector2f(parentPosition)
-    for(child<-children){
-      if(child!=null){
-        child.calculatePosition(currentPosition)
-        currentPosition.x+=child.size.x
-      }
-    }
+//    val currentPosition=new Vector2f(parentPosition)
+//    for(child<-children){
+//      if(child!=null){
+//        child.calculatePosition(currentPosition)
+//        currentPosition.x+=child.size.x
+//      }
+//    }
+    var totalChildrenWidth=0
+    for(child<-children)if(child!=null)totalChildrenWidth+=child.size.x
   }
   override def draw(context:DrawContext): Unit = {
     if (children == null) return
@@ -65,5 +74,11 @@ class Row(val children:Array[Widget]) extends StatelessWidget {
 }
 
 object Row{
-  def apply(children: Array[Widget]=null): Row = new Row(children)
+  def apply(
+             children: Array[Widget]=null,
+             mainAxisSize: MainAxisSize=MainAxisSize.expand,
+             crossAxisAlignment: CrossAxisAlignment=CrossAxisAlignment.center,
+             mainAxisAlignment: MainAxisAlignment=MainAxisAlignment.start
+           ): Row =
+    new Row(children,mainAxisAlignment,crossAxisAlignment,mainAxisSize)
 }
