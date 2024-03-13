e789647d33572e2717e210bf2dfb614d79c2249e
Uros <karaleicuu@gmail.com>
2024-03-13 16:59:06 +0100

    camera controller3 start

--- src/com/systemvi/engine/camera/CameraController3.scala
+++ src/com/systemvi/engine/camera/CameraController3.scala
@@ -1,5 +1,35 @@
 package com.systemvi.engine.camera
 
-class CameraController3 {
+import com.systemvi.engine.window.InputProcessor
+import org.lwjgl.glfw.GLFW
 
+class CameraController3 extends InputProcessor{
+  var focused:Boolean=false
+  var camera:Camera3=null
+  override def keyDown(key: Int, scancode: Int, mods: Int): Boolean = {
+    key match {
+      case GLFW.GLFW_KEY_Q=>
+      case _=>
+    }
+    true
+  }
+  override def keyUp(key: Int, scancode: Int, mods: Int): Boolean = {
+    true
+  }
+  override def mouseDown(button: Int, mods: Int, x: Double, y: Double): Boolean = {
+    focused=true
+    true
+  }
+  override def mouseUp(button: Int, mods: Int, x: Double, y: Double): Boolean = {
+    true
+  }
+  override def mouseMove(x: Double, y: Double): Boolean = {
+    true
+  }
+  override def scroll(offsetX: Double, offsetY: Double): Boolean = {
+    true
+  }
+  override def resize(width: Int, height: Int): Boolean = {
+    true
+  }
 }
