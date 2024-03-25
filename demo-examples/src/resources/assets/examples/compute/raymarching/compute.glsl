0964c976442bec0994f6101284c078ad09f550eb
Uros <karaleicuu@gmail.com>
2024-03-25 21:25:13 +0100

    setup for more compute shder ray marching

--- demo-examples/src/resources/assets/examples/compute/raymarching/compute.glsl
+++ demo-examples/src/resources/assets/examples/compute/raymarching/compute.glsl
@@ -2,6 +2,7 @@
 #define RAY_ITERATIONS 100
 #define DELTA_EPSILON 0.01
 #define MAX_DISTANCE 1000.0
+#define FIXED_MIN_TRAVEL_DISTANCE 0.2
 //in uvec3 gl_NumWorkGroups;
 //in uvec3 gl_WorkGroupID;
 //in uvec3 gl_LocalInvocationID;
@@ -69,21 +70,30 @@ vec3 translate(vec3 p,vec3 t){
     return p-t;
 }
 
+vec3 transform(in mat4 mat,in vec3 p){
+    vec4 helper=mat*vec4(p,1.0);
+    return helper.xyz;
+}
+
 float map(vec3 p){
     return unionSDF(
-        box(
-            translate(p,vec3(0.0,0.0,-10.0)),
-            vec3(1)
+        unionSDF(
+            box(
+                translate(p,vec3(0.0,0.0,-10.0)),
+                vec3(1)
+            ),
+            sphere(
+                translate(p,vec3(2.0,0.0,-10.0)),
+                1.0
+            )
         ),
-        sphere(
-            translate(p,vec3(2.0,0.0,-10.0)),
-            1
-        )
+        plane(p,vec3(0.0,1.0,0.0),3.0)
     );
 }
 vec4 color(vec3 p){
-    if(box(translate(p,vec3(0.0,0.0,-10.0)), vec3(1))<DELTA_EPSILON*2.0)return vec4(0.3,0.6,0.9,1.0);
-    if(box(translate(p,vec3(0.0,0.0,-10.0)), vec3(1))<DELTA_EPSILON*2.0)return vec4(0.3,0.6,0.9,1.0);
+    if(box(translate(p,vec3(0.0,0.0,-10.0)), vec3(1.0))<DELTA_EPSILON*2.0)return vec4(0.3,0.6,0.9,1.0);
+    if(sphere(translate(p,vec3(2.0,0.0,-10.0)), 1.0)<DELTA_EPSILON*2.0)return vec4(0.9,0.6,0.3,1.0);
+    if(plane(p,vec3(0.0,1.0,0.0),3.0)<DELTA_EPSILON*2.0)return vec4(0.6,0.8,0.3,1.0);
     return vec4(0.0);
 }
 
@@ -92,40 +102,76 @@ void rayMarch(out float distance,out vec3 endPoint,out int numOfIterations,in ve
     endPoint=vec3(rayOrigin);
     for(int i=0;i<RAY_ITERATIONS;i++){
         numOfIterations=i;
-        vec3 p=rayOrigin+distance*rayDirection;
-        float d=map(p);
+        float d=map(rayOrigin+distance*rayDirection);
         distance+=d;
-        if(distance>MAX_DISTANCE||d<DELTA_EPSILON)break;
+        if(distance>MAX_DISTANCE||d<DELTA_EPSILON){
+            numOfIterations=i;
+            endPoint=rayOrigin+distance*rayDirection;
+            return;
+        }
     }
+    numOfIterations=RAY_ITERATIONS;
     endPoint=rayOrigin+distance*rayDirection;
 }
+vec3 getNormal(vec3 p){
+    return normalize(vec3(
+        map(p+vec3(DELTA_EPSILON,0.0,0.0))-map(p-vec3(DELTA_EPSILON,0.0,0.0)),
+        map(p+vec3(0.0,DELTA_EPSILON,0.0))-map(p-vec3(0.0,DELTA_EPSILON,0.0)),
+        map(p+vec3(0.0,0.0,DELTA_EPSILON))-map(p-vec3(0.0,0.0,DELTA_EPSILON))
+    ));
+}
 
-vec4 getColor(vec2 uv,vec2 size){
+void phongData(){
+
+}
+void phongWithReflectionsData(){
+
+}
+void pbrData(){
+
+}
+vec4 phong(){
+    return vec4(0.0);
+}
+
+vec4 phongReflections(){
+    return vec4(0.0);
+}
+
+vec4 pbr(){
+    return vec4(0.0);
+}
+
+vec4 calculateColor(vec2 uv,vec2 size){
     //rayDir rayOrigin
     mat4 inverseView=inverse(view);
     vec4 helper;
+    float f=2.0;
 
-    vec3 focusPoint=vec3(0.0,0.0,1.0);
-    vec3 rayOrigin=vec3(uv,0.0);
-
-    helper=vec4(focusPoint,1.0);
-    helper=inverseView*helper;
-    focusPoint=helper.xyz;
-    helper=vec4(rayOrigin,1.0);
-    helper=inverseView*helper;
-    rayOrigin=helper.xyz;
-
+    vec3 cameraPosition=-view[3].xyz;
+    vec3 focusPoint=transform(inverseView,vec3(0.0,0.0,f));
+    vec3 rayOrigin=transform(inverseView,vec3(uv,0.0));
     vec3 rayDirection=normalize(rayOrigin-focusPoint);
 
-//    rayOrigin-=view[3].xyz;
-//    rayDirection=transpose(mat3(view))*rayDirection;
-
-    float distance;
-    vec3 point;
+    float sceneToCameraDistance;
+    vec3 firstHit;
     int numOfIterations;
-    rayMarch(distance,point,numOfIterations,rayOrigin,rayDirection);
+    rayMarch(sceneToCameraDistance,firstHit,numOfIterations,rayOrigin,rayDirection);
 
-    return vec4(vec3(float(numOfIterations)/float(RAY_ITERATIONS)),1.0);
+    vec4 firstHitColor=color(firstHit);
+    vec3 normal=getNormal(firstHit);
+
+    vec3 lightPosition=vec3(2.0,10.0,-8.0);
+    rayOrigin=firstHit-rayDirection*DELTA_EPSILON*2.0;
+    rayDirection=normalize(lightPosition-rayOrigin);
+    float sceneToLightDistance;
+    float lightDistance=length(firstHit-lightPosition);
+    vec3 secondHit;
+    rayMarch(sceneToLightDistance,secondHit,numOfIterations,rayOrigin,rayDirection);
+
+    vec4 finalColor=vec4(firstHitColor.rgb*mix(1.0,0.4,float(sceneToLightDistance<lightDistance)),1.0);
+
+    return finalColor;
 }
 
 
@@ -140,5 +186,5 @@ void main() {
     uv.x*=aspect;
     //uv size
 
-    imageStore(outputTexture,texelCoord,getColor(uv,size));
+    imageStore(outputTexture,texelCoord,calculateColor(uv,size));
 }
