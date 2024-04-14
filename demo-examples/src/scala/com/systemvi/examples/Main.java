62c5c8b014e5218bc0fb76e0802648ef5ae55f9d
Uros <karaleicuu@gmail.com>
2024-04-14 19:01:57 +0200

    random

--- demo-examples/src/scala/com/systemvi/examples/Main.java
+++ demo-examples/src/scala/com/systemvi/examples/Main.java
@@ -1,13 +1,15 @@
 package com.systemvi.examples;
 
 import com.systemvi.engine.utils.Utils;
-import com.systemvi.examples.compute.reactiondiffusion.App;
+import com.systemvi.examples.compute.raymarching.App;
+import com.systemvi.examples.uitest.WidgetTest;
 
 public class Main {
     public static void main(String[] args){
         Utils.assetsFolder="";
 //        new WidgetTest().run();
 //        new ContainerTest().run();
+//        new WidgetTest().run();
         new App().run();
 //        new NoiseTest(3,3,60,800,600,"ref").run();
     }
