e4fde06e676853b4d3454230c5b9d55173c76a2f
uros <karaleicu@gmail.com>
2024-04-23 19:39:33 +0200

    sdf nesto pocetak

--- src/scala/com/systemvi/engine/math/sdf/SDF.scala
+++ src/scala/com/systemvi/engine/math/sdf/SDF.scala
@@ -1,5 +1,13 @@
 package com.systemvi.engine.math.sdf
 
-class SDF {
+import org.joml.Vector3f
 
+class SDF {
+  def sphere(p:Vector3f, s: Float): Float = p.length()- s
+
+  def box(p: Vector3f, b: Vector3f): Float = {
+    val q = new Vector3f(p.absolute()).sub(b)
+    q.max(new Vector3f(0)).length()+Math.min(Math.max(q.x,Math.max(q.y,q.z)),0)
+  }
+  def union(d0:Float,d1:Float): Unit = Math.min(d0,d1)
 }
