f50bdaba9e5aa118cdba22655165a3d3d876de80
uros <karaleicu@gmail.com>
2024-04-24 20:53:44 +0200

    uspesno ucitavanje fonta i crtanje jednog karaktera

--- demo-examples/src/scala/com/systemvi/examples/uitest/WidgetRenderer2Test.scala
+++ demo-examples/src/scala/com/systemvi/examples/uitest/WidgetRenderer2Test.scala
@@ -14,10 +14,9 @@ import com.systemvi.engine.window.Window
 class WidgetRenderer2Test extends Game(3,3,60,800,600,"Widget renderer test"){
   var camera:Camera3=null
   var widgetRenderer:WidgetRenderer2=null
-//  var font:Font=null
+  var font:Font=null
   var textureRenderer:TextureRenderer=null
-  var regions:Array[Array[TextureRegion]]=null
-
+  var regions:Array[TextureRegion]=null
   override def setup(window: Window): Unit = {
     camera=Camera3.builder2d()
       .size(window.getWidth,window.getHeight)
@@ -25,8 +24,16 @@ class WidgetRenderer2Test extends Game(3,3,60,800,600,"Widget renderer test"){
       .scale(1,-1)
       .build()
     widgetRenderer=new WidgetRenderer2()
-//    font=new Font("assets/examples/widgetRenderer2Test/font.PNG","assets/examples/widgetRenderer2Test/font.json")
-//    regions=TextureRegion.split(font.texture,50,50)
+    font=Font.load(
+      "assets/examples/widgetRenderer2Test/font.PNG",
+      "assets/examples/widgetRenderer2Test/font.json"
+    )
+    regions=Array.ofDim(font.symbols.length)
+    font.symbols.zipWithIndex.foreach{
+      case (symbol:Font#Symbol,index:Int)=> regions(index)=new TextureRegion(
+        font.texture,symbol.x,symbol.y,symbol.width,symbol.height
+      )
+    }
     textureRenderer=new TextureRenderer()
     textureRenderer.view(camera.view)
     textureRenderer.projection(camera.projection)
@@ -34,7 +41,8 @@ class WidgetRenderer2Test extends Game(3,3,60,800,600,"Widget renderer test"){
   override def loop(delta: Float): Unit = {
     Utils.clear(1,0,0,1,Buffer.COLOR_BUFFER)
     Utils.enableBlending()
-//    textureRenderer.draw(font.texture,0,0,font.texture.getWidth,font.texture.getHeight)
+    val r=regions(font.symbols.indexOf(font.symbols.find(p=>p.id.toChar=='\\').get))
+    textureRenderer.draw(r,100,100,r.width,r.height)
     textureRenderer.flush()
     Utils.disableBlending()
   }
