dbe342735c04a4eb637efdd3b510fab988a02fed
Uros <karaleicuu@gmail.com>
2024-05-13 10:45:29 +0200

    samo ne sme da se koristi try with resource with aiScene ubicu se

--- src/scala/com/systemvi/engine/model/ModelLoader.java
+++ src/scala/com/systemvi/engine/model/ModelLoader.java
@@ -13,38 +13,38 @@ public class ModelLoader {
         File file=new File(params.fileName);
         System.out.println(file.exists());
         try{
-            try(AIScene aiScene = aiImportFile(params.fileName,aiProcess_JoinIdenticalVertices | aiProcess_Triangulate | aiProcess_FixInfacingNormals)){
-                if(aiScene == null){
-                    System.out.println("[ERROR] failed to load model");
-                    return model;
-                }
-                System.out.println("aiScene");
-                PointerBuffer meshesBuffer=aiScene.mMeshes();
-                int meshCount=aiScene.mNumMeshes();
-                for(int i=0; i<meshCount; i++){
-                    AIMesh aiMesh=AIMesh.create(meshesBuffer.get(i));
-                    System.out.println("\taiMesh");
-                    AIVector3D.Buffer positions=aiMesh.mVertices();
-                    while(positions.remaining()>0){
-                        AIVector3D position=positions.get();
-                        System.out.printf("\t\t%f, %f, %f\n",position.x(),position.y(),position.z());
-                    }
-                    AIVector3D.Buffer normals=aiMesh.mNormals();
-                    while(normals.remaining()>0){
-                        AIVector3D normal=normals.get();
-                        System.out.printf("\t\t%f, %f, %f\n",normal.x(),normal.y(),normal.z());
-                    }
-                }
-
-                int materialCount=aiScene.mNumMaterials();
-                PointerBuffer materialsBuffer=aiScene.mMaterials();
-                for(int i=0; i<materialCount; i++){
-                    AIMaterial aiMaterial=AIMaterial.create(materialsBuffer.get(i));
-                    System.out.println("\taiMaterial");
-                }
-
-                model=new Model();
+            AIScene aiScene = aiImportFile(params.fileName,aiProcess_JoinIdenticalVertices | aiProcess_Triangulate | aiProcess_FixInfacingNormals);
+            if(aiScene == null){
+                System.out.println("[ERROR] failed to load model");
+                return model;
             }
+            System.out.println("aiScene");
+            PointerBuffer meshesBuffer=aiScene.mMeshes();
+            int meshCount=aiScene.mNumMeshes();
+            for(int i=0; i<meshCount; i++){
+                AIMesh aiMesh=AIMesh.create(meshesBuffer.get(i));
+                System.out.println("\taiMesh");
+                AIVector3D.Buffer positions=aiMesh.mVertices();
+                while(positions.remaining()>0){
+                    AIVector3D position=positions.get();
+                    System.out.printf("\t\t%f, %f, %f\n",position.x(),position.y(),position.z());
+                }
+                AIVector3D.Buffer normals=aiMesh.mNormals();
+                while(normals.remaining()>0){
+                    AIVector3D normal=normals.get();
+                    System.out.printf("\t\t%f, %f, %f\n",normal.x(),normal.y(),normal.z());
+                }
+            }
+
+            int materialCount=aiScene.mNumMaterials();
+            PointerBuffer materialsBuffer=aiScene.mMaterials();
+            for(int i=0; i<materialCount; i++){
+                AIMaterial aiMaterial=AIMaterial.create(materialsBuffer.get(i));
+                System.out.println("\taiMaterial");
+            }
+
+            model=new Model();
+
         }catch (Exception e){
             e.printStackTrace();
         }
