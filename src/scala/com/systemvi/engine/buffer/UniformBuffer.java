2395d8bb7e13f6408bf1226b5328b6a64b7b1e2d
uros <karaleicu@gmail.com>
2024-05-19 22:02:32 +0200

    uniform buffer object kao radi

--- src/scala/com/systemvi/engine/buffer/UniformBuffer.java
+++ src/scala/com/systemvi/engine/buffer/UniformBuffer.java
@@ -1,5 +1,7 @@
 package com.systemvi.engine.buffer;
 
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import org.joml.Vector4f;
 
 import static org.lwjgl.opengl.GL33.*;
@@ -16,24 +18,55 @@ public class UniformBuffer {
         unbind();
     }
 
-    public void setSubData(int offset,int[] data){
+    public void setSubData(int byteOffset,int[] data){
         bind();
-        glBufferSubData(GL_UNIFORM_BUFFER,offset,data);
+        glBufferSubData(GL_UNIFORM_BUFFER,byteOffset,data);
         unbind();
     }
-    public void setSubData(int offset,short[] data){
+    public void setSubData(int byteOffset,short[] data){
         bind();
-        glBufferSubData(GL_UNIFORM_BUFFER,offset,data);
+        glBufferSubData(GL_UNIFORM_BUFFER,byteOffset,data);
         unbind();
     }
-    public void setSubData(int offset,float[] data){
+    public void setSubData(int byteOffset,float[] data){
         bind();
-        glBufferSubData(GL_UNIFORM_BUFFER,offset,data);
+        glBufferSubData(GL_UNIFORM_BUFFER,byteOffset,data);
         unbind();
     }
-    public void setSubData(int offset, Vector4f[] data){
+    public void setSubData(int byteOffset, Vector4f[] data){
         bind();
-//        glBufferSubData(GL_UNIFORM_BUFFER,offset,data);
+        float[] buffer = new float[data.length*4];
+        for(int i=0;i<data.length;i++){
+            buffer[i*4] = data[i].x;
+            buffer[i*4+1] = data[i].y;
+            buffer[i*4+2] = data[i].z;
+            buffer[i*4+3] = data[i].w;
+        }
+        glBufferSubData(GL_UNIFORM_BUFFER,byteOffset,buffer);
+        unbind();
+    }
+    public void setSubData(int byteOffset, Vector3f[] data){
+        bind();
+        float[] buffer = new float[data.length*3];
+        for(int i=0;i<data.length;i++){
+            buffer[i*4] = data[i].x;
+            buffer[i*4+1] = data[i].y;
+            buffer[i*4+2] = data[i].z;
+        }
+        glBufferSubData(GL_UNIFORM_BUFFER,byteOffset,buffer);
+        unbind();
+    }
+    public void setSubData(int byteOffset, Matrix4f[] data){
+        bind();
+        float[] buffer = new float[data.length*16];
+        for(int i=0;i<data.length;i++){
+            for(int j=0;j<16;j++){
+                float[] f=new float[16];
+                data[i].get(f);
+                buffer[i*16+j] = f[j];
+            }
+        }
+        glBufferSubData(GL_UNIFORM_BUFFER,byteOffset,buffer);
         unbind();
     }
 
