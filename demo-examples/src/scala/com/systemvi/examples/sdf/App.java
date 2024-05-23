b92b6065e5ef7847112017f1b7c6ca5a052e0a61
Uros <karaleicuu@gmail.com>
2024-05-23 13:36:57 +0200

    radi jej

--- demo-examples/src/scala/com/systemvi/examples/sdf/App.java
+++ demo-examples/src/scala/com/systemvi/examples/sdf/App.java
@@ -29,90 +29,13 @@ public class App extends Game {
     Texture texture;
     Camera3 camera, worldCamera;
     TextureRenderer renderer;
-    final float Epsilon = 0.001f;
-    final float focalLength=2.2f;
-    public Random r=new Random();
+    public Random r = new Random();
     public TextureData data;
     public CameraController3 controller;
     public final int tasks=8,threads=8;
     ExecutorService service;
     Future[] futures;
     public RayMarchRenderer rayMarchRenderer;
-
-    public Vector3f RayMarch(Vector3f ro, Vector3f rd,int iterations){
-        float d = 0;
-        for (int k = 0; k < iterations; k++) {
-            Vector3f p = new Vector3f(
-                ro.x + rd.x * d,
-                ro.y + rd.y * d,
-                ro.z + rd.z * d
-            );
-            d += Map.getDistance(p);
-            if (d > 1000) break;
-            if (Map.getDistance(p) < Epsilon) break;
-        }
-        return new Vector3f(
-            ro.x + rd.x * d,
-            ro.y + rd.y * d,
-            ro.z + rd.z * d
-        );
-    }
-
-    public Vector3f getNormal(Vector3f p){
-        return new Vector3f(
-            Map.getDistance(new Vector3f(p.x + 0.01f, p.y, p.z)) - Map.getDistance(new Vector3f(p.x - 0.01f, p.y, p.z)),
-            Map.getDistance(new Vector3f(p.x, p.y + 0.01f, p.z)) - Map.getDistance(new Vector3f(p.x, p.y - 0.01f, p.z)),
-            Map.getDistance(new Vector3f(p.x, p.y, p.z + 0.01f)) - Map.getDistance(new Vector3f(p.x, p.y, p.z - 0.01f))
-        ).normalize();
-    }
-
-    public Vector4f SimulatePhoton(float x, float y, int bounces, Random r,int iterations){
-        Vector4f color = new Vector4f(1);
-        Vector3f[] ro = new Vector3f[bounces + 1];
-        Vector3f[] rd = new Vector3f[bounces + 1];
-
-        Matrix4f inverted=new Matrix4f(worldCamera.view()).invert();
-        Vector4f focus=new Vector4f(0,0,focalLength,1).mul(inverted);
-        Vector4f point=new Vector4f(x,y,0,1).mul(inverted);
-
-        ro[0] = new Vector3f(focus.x,focus.y,focus.z);
-        rd[0] = new Vector3f(point.x,point.y,point.z).sub(ro[0]).normalize();
-
-        for (int k = 0; k < bounces; k++) {
-            Vector3f p = RayMarch(ro[k], rd[k],iterations);
-            Vector3f normal = getNormal(p);
-            Material m = Map.getMaterial(p);
-            Vector4f c = m.color();
-
-            if (p.distance(ro[k]) > 1000)
-                break;
-
-            ro[k + 1] = new Vector3f(p).add(normal.x * 2 * Epsilon, normal.y * 2 * Epsilon, normal.z * 2 * Epsilon);
-            rd[k + 1] = new Vector3f(rd[k]).reflect(normal).add(new Vector3f(r.nextFloat() * 2 - 1, r.nextFloat() * 2 - 1, r.nextFloat() * 2 - 1).mul(m.roughness())).normalize();
-            if (r.nextFloat() < m.metallic())
-                color.mul(c);
-        }
-        return color;
-    }
-    public Vector4f calculatePixel(int i,int j,int bounces,int samples,int iterations){
-        float x, y;
-        x = i;
-        x /= texture.getWidth();
-        x = x*2-1;
-        x *= (float)texture.getWidth() / (float)texture.getHeight();
-        y = j;
-        y /= texture.getHeight();
-        y = 2*y-1;
-        y *= -1;
-
-        Vector4f color = new Vector4f(0);
-
-        for (int k = 0; k < samples; k++) {
-            color.add(SimulatePhoton(x, y, bounces, r,iterations));
-        }
-        color.div(samples);
-        return color;
-    }
 
     @Override
     public void setup(Window window) {
@@ -143,7 +66,7 @@ public class App extends Game {
         service = Executors.newFixedThreadPool(threads);
         futures=new Future[tasks];
 
-        rayMarchRenderer=RayMarchRenderer.apply(camera);
+        rayMarchRenderer=RayMarchRenderer.apply(worldCamera);
 
         startRender(4,10,500);
     }
