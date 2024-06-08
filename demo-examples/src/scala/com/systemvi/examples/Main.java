cc626f3597331c5b840377e1448986f5eb7e0680
Uros <karaleicuu@gmail.com>
2024-06-08 15:19:22 +0200

    dominion example

--- demo-examples/src/scala/com/systemvi/examples/Main.java
+++ demo-examples/src/scala/com/systemvi/examples/Main.java
@@ -1,7 +1,7 @@
 package com.systemvi.examples;
 
 import com.systemvi.engine.utils.Utils;
-import com.systemvi.examples.compute.raymarching.App;
+import com.systemvi.examples.ecs.App;
 
 public class Main {
     public static void main(String[] args) throws Exception{
