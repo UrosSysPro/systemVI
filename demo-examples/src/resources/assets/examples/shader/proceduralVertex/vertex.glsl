5b5308a18be9d9942f983d681a683d7078420694
uros <karaleicu@gmail.com>
2024-06-14 00:56:04 +0200

    kao radi

--- demo-examples/src/resources/assets/examples/shader/proceduralVertex/vertex.glsl
+++ demo-examples/src/resources/assets/examples/shader/proceduralVertex/vertex.glsl
@@ -1,13 +1,20 @@
 #version 460 core
 
+uniform ivec2 grid;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main(){
-    vec4 points[3]={
-        vec4( 0.0, 0.5,0.0,1.0),
-        vec4(-0.5,-0.5,0.0,1.0),
-        vec4( 0.5,-0.5,0.0,1.0),
+    vec4 points[2]={
+        vec4(0,0,0,1),
+        vec4(0,1,0,1)
     };
-    gl_Position=projection*view*points[gl_VertexID];
+
+    ivec2 quadrant;
+    quadrant.x=min(gl_VertexID%(2*grid.x+1),2*grid.x-1)/2;
+    quadrant.y=gl_VertexID/(2*grid.x+1);
+    int index=min(gl_VertexID%(2*grid.x+1),2*grid.x-1)%2;
+
+
+    gl_Position=projection*view*(points[index]+vec4(vec2(quadrant),0.0,0.0));
 }
--- demo-examples/src/scala/com/systemvi/examples/shader/App.scala
+++ demo-examples/src/scala/com/systemvi/examples/shader/App.scala
@@ -8,11 +8,13 @@ import com.systemvi.engine.ui.utils.data.Colors
 import com.systemvi.engine.utils.Utils
 import com.systemvi.engine.utils.Utils.Buffer
 import com.systemvi.engine.window.Window
+import org.joml.Vector2i
 
 class App extends Game(4,6,60,800,600,"Shader"){
   var shader:Shader=null
   var vertexArray:VertexArray=null
   var controller:CameraController3=null
+  val grid=new Vector2i(4,4)
 
   override def setup(window: Window): Unit = {
     vertexArray=new VertexArray()
@@ -23,17 +25,20 @@ class App extends Game(4,6,60,800,600,"Shader"){
     controller=CameraController3.builder()
       .window(window)
       .camera(Camera3.builder3d().build())
+      .aspect(window.getWidth.toFloat/window.getHeight.toFloat)
       .build()
     setInputProcessor(controller)
   }
 
   override def loop(delta: Float): Unit = {
     Utils.clear(Colors.blue500,Buffer.COLOR_BUFFER)
+    Utils.enableLines(2)
     controller.update(delta)
     shader.use()
     vertexArray.bind()
+    shader.setUniform("grid",grid)
     shader.setUniform("view",controller.camera.view)
     shader.setUniform("projection",controller.camera.projection)
-    shader.drawArrays(Primitive.TRIANGLES,3)
+    shader.drawArrays(Primitive.TIRANGLE_STRIP,(grid.x*2+1)*(grid.y-1))
   }
 }
