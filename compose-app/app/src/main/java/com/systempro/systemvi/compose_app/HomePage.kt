396aedf2f56ee45a828f6d5fd97b90c0d0c20143
Uros <karaleicuu@gmail.com>
2024-08-05 12:45:55 +0200

    nothing much

--- compose-app/app/src/main/java/com/systempro/systemvi/compose_app/HomePage.kt
+++ compose-app/app/src/main/java/com/systempro/systemvi/compose_app/HomePage.kt
@@ -2,13 +2,16 @@ package com.systempro.systemvi.compose_app
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,28 +24,56 @@ fun HomePage(
     }else{
         if(dynamicColor) dynamicLightColorScheme(LocalContext.current) else Theme.LightColorScheme
     }
+    val drawerState= rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope= rememberCoroutineScope()
+    val (state,setState) = remember {mutableStateOf("text")}
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Theme.Typography,
-        content = {
+        typography = Theme.Typography
+    ) {
+        ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
+            ModalDrawerSheet {
+                Text(text = "eee")
+                Text(text = "eee")
+                Text(text = "eee")
+                Text(text = "eee")
+                NavigationDrawerItem(label = { Text(text = "eee") }, selected = false, onClick = { print("eee") })
+            }
+        }) {
             Scaffold(
-                modifier = Modifier,
-                topBar = {TopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                    title = {Text(text="App Bar")}
-                )},
+                modifier = Modifier.padding(10.dp),
+                topBar = {
+                    TopAppBar(
+                        actions = {
+                            Button(onClick = { scope.launch{drawerState.open()} }) {
+                                Text(text = "ee")
+                            }
+                        },
 
-                content = {innerPadding-> Column(modifier = Modifier.padding(innerPadding)) {
-                    for(index in 0 until 10){
-                        Text(text = "Item $index")
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                        title = { Text(text = "App Bar aaa") }
+                    )
+                },
+
+                content = { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            ,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        for (index in 0 until 10) {
+                            Text(text = "Item $index")
+                        }
                     }
-                }}
+                }
             )
         }
-    )
+    }
 }
 
 @Preview(showSystemUi = true, name = "Home page test")
