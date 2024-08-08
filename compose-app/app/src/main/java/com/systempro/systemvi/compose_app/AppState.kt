241af0d532f32be7990e57a9a7a20d7094f43f97
Uros <karaleicuu@gmail.com>
2024-08-08 12:39:19 +0200

    compose app navigation

--- compose-app/app/src/main/java/com/systempro/systemvi/compose_app/AppState.kt
+++ compose-app/app/src/main/java/com/systempro/systemvi/compose_app/AppState.kt
@@ -1,21 +1,14 @@
 package com.systempro.systemvi.compose_app
-
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 
 class AppState : ViewModel() {
-    var counter by mutableIntStateOf(0)
     val httpClient: HttpClient = HttpClient(CIO)
+    var useLightTheme by mutableStateOf(false)
+    var useDynamicTheme by mutableStateOf(false)
 
-    init {
-        counter = 10
-    }
-
-    fun add() {
-        counter++
-    }
 }
--- compose-app/app/src/main/java/com/systempro/systemvi/compose_app/HomePage.kt
+++ compose-app/app/src/main/java/com/systempro/systemvi/compose_app/HomePage.kt
@@ -3,37 +3,49 @@ package com.systempro.systemvi.compose_app
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import io.ktor.client.request.*
 import kotlinx.coroutines.launch
 
-data class State(var a:Int,var b:String)
+data class NavigationLocation(
+    val id:Int,
+    val name:String,
+    val builder:@Composable ()->Unit,
+    val icon:ImageVector
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomePage(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false
-) {
-    val colorScheme = if(darkTheme){
-        if(dynamicColor) dynamicDarkColorScheme(LocalContext.current) else Theme.DarkColorScheme
-    }else{
-        if(dynamicColor) dynamicLightColorScheme(LocalContext.current) else Theme.LightColorScheme
+fun HomePage(appState:AppState = viewModel()) {
+    val lightTheme=appState.useLightTheme
+    val dynamicTheme=appState.useDynamicTheme
+    val colorScheme=when{
+        dynamicTheme  &&  lightTheme -> dynamicLightColorScheme(LocalContext.current)
+        dynamicTheme  && !lightTheme -> dynamicDarkColorScheme(LocalContext.current)
+        !dynamicTheme &&  lightTheme -> Theme.LightColorScheme
+        /*!dynamic !light*/     else -> Theme.DarkColorScheme
     }
-    val drawerState= rememberDrawerState(initialValue = DrawerValue.Closed)
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
-    var clicks by rememberSaveable{ mutableIntStateOf(0) }
-    var customState by remember { mutableStateOf(State(0,"hello")) }
-    val appState=viewModel<AppState>()
 
+    val navigationItems:List<NavigationLocation> = listOf(
+        NavigationLocation(0,"Theme",{ Box(modifier = Modifier){Text("settings page")} },Icons.Rounded.Settings),
+        NavigationLocation(1,"Theme",{ Box(modifier = Modifier)},Icons.Rounded.Settings),
+        NavigationLocation(2,"Theme",{ Box(modifier = Modifier)},Icons.Rounded.Settings),
+        NavigationLocation(3,"Theme",{ Box(modifier = Modifier)},Icons.Rounded.Settings),
+    )
+
+    var pageIndex by rememberSaveable { mutableIntStateOf(0) }
 
     MaterialTheme(
         colorScheme = colorScheme,
@@ -42,14 +54,15 @@ fun HomePage(
         ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
             ModalDrawerSheet {
                 Box(Modifier.padding(horizontal = 15.dp, vertical = 15.dp)) {
-                    Text(text = "Navigation Drawer",)
+                    Text(text = "Navigation Drawer", style = MaterialTheme.typography.headlineMedium)
                 }
-                LazyColumn {
-                    items(10, key = {it}){
+                LazyColumn(modifier = Modifier.padding(horizontal = 10.dp)) {
+                    items(navigationItems.size, key = {navigationItems[it].id}){
+                        val navigationItem=navigationItems[it]
                         NavigationDrawerItem(
-                            label = { Text(text = "eee") },
-                            selected = false,
-                            onClick = { print("eee") }
+                            label = {Text(navigationItem.name)},
+                            selected = navigationItem.id==pageIndex,
+                            onClick = {pageIndex=it}
                         )
                     }
                 }
@@ -58,50 +71,45 @@ fun HomePage(
             Scaffold(
                 topBar = {
                     TopAppBar(
-                        navigationIcon = { Button(onClick ={ scope.launch { drawerState.open() }}) {
-                            Text(text = "open")
-                        }},
+                        navigationIcon = { IconButton(
+                            onClick = { scope.launch { drawerState.open() } },
+                            content = { Icon(Icons.Rounded.Menu, contentDescription = "Menu") }
+                        )},
                         actions = {
-                            Button(onClick = { scope.launch{drawerState.open()} }) {
-                                Text(text = "ee")
-                            }
+                            IconButton(
+                                onClick = { scope.launch { drawerState.open() } },
+                                content = { Icon(Icons.Rounded.Search, contentDescription = "Menu") }
+                            )
+                            IconButton(
+                                onClick = { scope.launch { drawerState.open() } },
+                                content = { Icon(Icons.Rounded.MoreVert, contentDescription = "Menu") }
+                            )
                         },
-
-                        colors = TopAppBarDefaults.topAppBarColors(
-                            containerColor = MaterialTheme.colorScheme.primaryContainer,
-                            titleContentColor = MaterialTheme.colorScheme.primary,
-                        ),
-                        title = { Text(text = "App Bar") }
+//                        colors = TopAppBarDefaults.topAppBarColors(
+//                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                            titleContentColor = MaterialTheme.colorScheme.primary,
+//                        ),
+                        title = { Text(text = "App Bar ") }
                     )
                 },
 
                 content = { innerPadding ->
                     Column(modifier = Modifier
                         .padding(innerPadding)
-                        .fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "clicks:$clicks")
-                        Button(onClick = {clicks++}) {
-                            Text(text = "add")
-                        }
-                        Text(text = "text: ${customState.b} counter: ${customState.a}")
-                        Button(onClick = {
-                            customState=State(customState.a+1,"hello")
-                            println("customState: ${customState.a}")
-                        }) {
-                            SideEffect {
-                                Logger.debug("side effect print")
-                            }
-                            LaunchedEffect(Unit){
-                                Logger.debug("launched effect print")
-                            }
-                            Text(text = "add to custom state")
-                        }
-                        Text(text = "appState.counter: ${appState.counter}")
-                        Button(onClick = {
-                            appState.add()
-                        }) {
-                            Text(text = "add to app state counter")
-                        }
+                        .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text("dynamic mode")
+                        Switch(
+                            checked = appState.useDynamicTheme,
+                            onCheckedChange = {value->appState.useDynamicTheme = value}
+                        )
+                        Text("light mode")
+                        Switch(
+                            checked = appState.useLightTheme,
+                            onCheckedChange = {value->appState.useLightTheme = value}
+                        )
                     }
                 }
             )
@@ -112,5 +120,5 @@ fun HomePage(
 @Preview(showSystemUi = true, name = "Home page test")
 @Composable
 fun HomePagePreview() {
-    HomePage(dynamicColor = true, darkTheme = true)
+    HomePage()
 }
