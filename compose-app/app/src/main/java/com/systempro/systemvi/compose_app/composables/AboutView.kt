2d49bdfe345937f243bfe2ae4c6636e65d605a4d
Uros <karaleicuu@gmail.com>
2024-08-14 13:46:50 +0200

    compose app about page, nista

--- compose-app/app/src/main/java/com/systempro/systemvi/compose_app/composables/AboutView.kt
+++ compose-app/app/src/main/java/com/systempro/systemvi/compose_app/composables/AboutView.kt
@@ -1,9 +1,14 @@
 package com.systempro.systemvi.compose_app.composables
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.systempro.systemvi.compose_app.utils.Logger
 
 @Composable fun AboutView() {
@@ -13,9 +18,15 @@ import com.systempro.systemvi.compose_app.utils.Logger
             Logger.debug("leave about page")
         }
     }
-    LazyColumn(){
-        item {
-            Text("About page")
+    Box(modifier = Modifier.fillMaxSize()){
+        LazyColumn(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.fillMaxSize()
+        ){
+            for(i in 0 until 100)item {
+                Text("item $i")
+            }
         }
     }
 }
