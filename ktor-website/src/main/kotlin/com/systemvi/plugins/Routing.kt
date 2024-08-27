c4957f05842ae4764499d1c0d5f654da61a9762b
Uros <karaleicuu@gmail.com>
2024-08-27 15:12:05 +0200

    serve static html and images

--- ktor-website/src/main/kotlin/com/systemvi/plugins/Routing.kt
+++ ktor-website/src/main/kotlin/com/systemvi/plugins/Routing.kt
@@ -1,16 +1,22 @@
 package com.systemvi.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("html","html")
+        staticResources("images","images")
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("[GET] root")
+        }
+        post("/"){
+            call.respondText("[POST] root aaa")
         }
         get("/about"){
-            call.respondText("oce ovo da  ee" )
+            call.respondText("[GET] about")
         }
     }
 }
