c33aacc08e7a902fbf4a281bf9990b132a710382
Uros <karaleicuu@gmail.com>
2024-09-09 14:41:17 +0200

    Linked list start

--- examples/data-structures/src/scala/com/systemvi/data_structures/Main.scala
+++ examples/data-structures/src/scala/com/systemvi/data_structures/Main.scala
@@ -2,6 +2,6 @@ package com.systemvi.data_structures
 
 object Main{
   def main(args: Array[String]): Unit = {
-    print("hello")
+
   }
 }
---
+++ examples/data-structures/src/scala/com/systemvi/data_structures/lists/LinkedList.java
@@ -0,0 +1,24 @@
+package com.systemvi.data_structures.lists;
+
+public class LinkedList {
+
+
+    public void addFirst(int value){
+
+    }
+    public void addLast(int value){
+
+    }
+    public void removeFirst(){
+
+    }
+    public void removeLast(){
+
+    }
+    public void add(int value,int index){
+
+    }
+    public int size(){
+        return 0;
+    }
+}
