691e03f2ea37911032377ac75e59640dbf511983
Uros Karaleic <karaleicu@gmail.com>
2024-09-23 12:08:27 +0200

    java iterable to scala iterable

--- examples/collections/src/scala/com/systemvi/collections/Main.scala
+++ examples/collections/src/scala/com/systemvi/collections/Main.scala
@@ -2,15 +2,16 @@ package com.systemvi.collections
 
 import com.systemvi.collections.lists.LinkedList
 
+import scala.jdk.CollectionConverters._
+
 object Main{
   def main(args: Array[String]): Unit = {
     val list=new LinkedList[Int]()
     for(i<-0 until 10){
       list.addLast(i)
     }
-    val iterator=list.iterator()
-    while(iterator.hasNext){
-      println(iterator.next())
+    for(item<-list.asScala){
+      println(item)
     }
   }
 }
