9cac7190cc4ad3780904960d0b4ec402fa323817
Uros Karaleic <karaleicu@gmail.com>
2024-09-30 15:29:34 +0200

    normal mapping subproject

--- examples/normal-mapping/src/scala/com/systemvi/normal_mapping/App2.scala
+++
@@ -1,5 +0,0 @@
-package com.systemvi.normal_mapping
-
-class App2 {
-
-}
--- examples/normal-mapping/src/scala/com/systemvi/normal_mapping/Main.scala
+++ examples/normal-mapping/src/scala/com/systemvi/normal_mapping/Main.scala
@@ -2,6 +2,6 @@ package com.systemvi.normal_mapping
 
 object Main{
   def main(args: Array[String]): Unit = {
-    print("hello")
+    new App().run()
   }
 }
