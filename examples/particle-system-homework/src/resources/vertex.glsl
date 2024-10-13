b4ea0633a8b9b2db3b998ab967080fa0855f11c7
Uros Karaleic <karaleicu@gmail.com>
2024-10-13 18:21:42 +0200

    colors

--- examples/particle-system-homework/src/resources/vertex.glsl
+++ examples/particle-system-homework/src/resources/vertex.glsl
@@ -5,7 +5,11 @@ uniform mat4 view;
 uniform mat4 projection;
 
 layout(location=0)in vec2 position;
+layout(location=1)in vec4 aColor;
+
+out vec4 vColor;
 
 void main(){
+    vColor=aColor;
     gl_Position=projection*view*vec4(position,0.0,1.0);
 }
