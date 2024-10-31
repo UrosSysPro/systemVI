79b78fab98c2df42455a4fda9dd84dfb8d461dee
Uros Karaleic <karaleicu@gmail.com>
2024-10-31 13:04:43 +0100

    texture builder

--- engine/src/scala/com/systemvi/engine/texture/Texture.java
+++ engine/src/scala/com/systemvi/engine/texture/Texture.java
@@ -3,6 +3,7 @@ package com.systemvi.engine.texture;
 import static org.lwjgl.opengl.GL33.*;
 import static org.lwjgl.opengl.GL42.glBindImageTexture;
 
+import com.systemvi.engine.ui.utils.data.Colors;
 import com.systemvi.engine.utils.Utils;
 import org.joml.Vector4f;
 import org.lwjgl.BufferUtils;
@@ -69,8 +70,8 @@ public class Texture {
         this.width = width;
         this.height = height;
         this.format = format;
-        setSamplerFilter(filterMin,filterMag);
-        setRepeat(horizontalRepeat,verticalRepeat);
+        setSamplerFilter(filterMin, filterMag);
+        setRepeat(horizontalRepeat, verticalRepeat);
         this.multisampled = multisampled;
         setBorderColor(borderColor);
     }
@@ -140,6 +141,17 @@ public class Texture {
         glTexImage2D(GL_TEXTURE_2D, 0, this.format.id, this.width, this.height, 0, this.format.id, GL_UNSIGNED_BYTE, buffer);
         glGenerateMipmap(GL_TEXTURE_2D);
         STBImage.stbi_image_free(buffer);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        return this;
+    }
+
+    public Texture fromFormat(int width, int height, Format format,boolean multisampled) {
+        this.width = width;
+        this.height = height;
+        this.format = format;
+        this.multisampled = multisampled;
+        glTexImage2D(GL_TEXTURE_2D, 0, this.format.id, this.width, this.height, 0, Format.RGBA.id, GL_UNSIGNED_BYTE, (ByteBuffer) null);
+        glGenerateMipmap(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, 0);
         return this;
     }
@@ -267,7 +279,78 @@ public class Texture {
 
     //builder
     public static class Builder {
+        private int width = 1;
+        private int height = 1;
+        private Format format = Format.RGBA;
+        private FilterMin filterMin = FilterMin.NEAREST;
+        private FilterMag filterMag = FilterMag.NEAREST;
+        private Repeat horizontalRepeat = Repeat.REPEAT, verticalRepeat = Repeat.REPEAT;
+        private boolean multisampled = false;
+        private final Vector4f borderColor = new Vector4f(Colors.black());
+        private String file = null;
 
+        public Builder width(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public Builder height(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder format(Format format) {
+            this.format = format;
+            return this;
+        }
+
+        public Builder filterMin(FilterMin filterMin) {
+            this.filterMin = filterMin;
+            return this;
+        }
+
+        public Builder filterMag(FilterMag filterMag) {
+            this.filterMag = filterMag;
+            return this;
+        }
+
+        public Builder horizontalRepeat(Repeat horizontalRepeat) {
+            this.horizontalRepeat = horizontalRepeat;
+            return this;
+        }
+
+        public Builder verticalRepeat(Repeat verticalRepeat) {
+            this.verticalRepeat = verticalRepeat;
+            return this;
+        }
+
+        public Builder multisampled(boolean multisampled) {
+            this.multisampled = multisampled;
+            return this;
+        }
+
+        public Builder borderColor(Vector4f borderColor) {
+            this.borderColor.set(borderColor);
+            return this;
+        }
+
+        public Builder file(String file) {
+            this.file = file;
+            return this;
+        }
+
+        public Texture build() {
+            Texture texture = new Texture();
+            if(file != null) {
+                texture.loadFromFile(file);
+            }else{
+                texture.fromFormat(width,height,format,multisampled);
+            }
+            texture.setRepeat(horizontalRepeat, verticalRepeat);
+            texture.setSamplerFilter(filterMin, filterMag);
+            texture.setBorderColor(borderColor);
+            return texture;
+        }
     }
 
     public static Builder builder() {
