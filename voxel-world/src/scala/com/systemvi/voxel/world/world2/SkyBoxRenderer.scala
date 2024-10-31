6685537f02bf4f488cceab2b7699699d7ef110ff
uros <karaleicu@gmail.com>
2024-11-01 00:21:58 +0100

    skybox renderer start

---
+++ voxel-world/src/scala/com/systemvi/voxel/world/world2/SkyBoxRenderer.scala
@@ -0,0 +1,69 @@
+package com.systemvi.voxel.world.world2
+
+import com.systemvi.engine.buffer.{ArrayBuffer, ElementsBuffer, VertexArray}
+import com.systemvi.engine.model.VertexAttribute
+
+
+class SkyBoxMesh {
+  private val vertexArray = VertexArray()
+  private val arrayBuffer = ArrayBuffer()
+  private val elementBuffer = ElementsBuffer()
+
+  vertexArray.bind()
+  arrayBuffer.bind()
+
+  arrayBuffer.setVertexAttributes(Array(VertexAttribute("aPos", 3)))
+  arrayBuffer.setData(Array(
+    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, // triangle 1 : end
+    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
+  ))
+
+  def bind(): Unit = arrayBuffer.bind()
+}
+
+class SkyBoxRenderer {
+
+}
