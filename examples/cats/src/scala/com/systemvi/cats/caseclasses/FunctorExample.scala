e391f24d060cb779fe6cf75fab701b2b67b9a838
Uros Karaleic <karaleicu@gmail.com>
2024-11-22 15:07:49 +0100

    radi functor to transform tree

--- examples/cats/src/scala/com/systemvi/cats/caseclasses/FunctorExample.scala
+++ examples/cats/src/scala/com/systemvi/cats/caseclasses/FunctorExample.scala
@@ -7,7 +7,7 @@ case class Node[F](value: F, left: Node[F]=null, right: Node[F]=null)
 
 object FunctorExample {
 
-  given Show[Node[Int]] = (node: Node[Int]) => {
+  given Show[Node[?]] = (node: Node[?]) => {
     def show[F](padding: String, node: Node[F]): String =
       if node != null then
         s"""${node.value}
@@ -18,8 +18,32 @@ object FunctorExample {
     show("", node)
   }
 
+//  given Show[Node[String]] = (node: Node[String]) => {
+//    def show[F](padding: String, node: Node[F]): String =
+//      if node != null then
+//        s"""${node.value}
+//           |$padding${show(padding ++ "\t", node.left)}
+//           |$padding${show(padding ++ "\t", node.right)}""".stripMargin
+//      else "null"
+//
+//    show("", node)
+//  }
+
+  given Functor[Node] = new Functor[Node]:
+    override def map[A, B](fa: Node[A])(f: A => B): Node[B] = {
+      if(fa!=null){
+        Node[B](
+          f(fa.value),
+          map(fa.left)(f),
+          map(fa.right)(f)
+        )
+      }else{
+        null
+      }
+    }
+
   def main(args: Array[String]): Unit = {
-    val tree = Node(1,
+    val tree:Node[?] = Node(1,
       Node(2,
         Node(10,
           Node(10),
@@ -30,7 +54,7 @@ object FunctorExample {
         Node(3),
         Node(3),
       ),
-    )
+    ).map(a=>s"value: $a")
     println(tree.show)
   }
 }
