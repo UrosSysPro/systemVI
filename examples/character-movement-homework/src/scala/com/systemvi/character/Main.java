721572a33b6b4eed44316c6cca06bdb7df2e3a57
Uros Karaleic <karaleicu@gmail.com>
2024-11-24 17:45:16 +0100

    reading level.txt

--- examples/character-movement-homework/src/scala/com/systemvi/character/Main.java
+++ examples/character-movement-homework/src/scala/com/systemvi/character/Main.java
@@ -16,17 +16,27 @@ import org.joml.Vector2i;
 import org.lwjgl.glfw.GLFW;
 import org.w3c.dom.UserDataHandler;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main extends Game {
     
     public Main(){
         super(3,3,60,800,600,"Character");
     }
-    
+
     Shape2Renderer2 renderer2;
     Camera3 camera;
     Texture texture;
     TextureRegion[][] regions;
     TextureRegion character,plant;
+
+    int tileSize=40;
+
+    List<List<Integer>> map;
 
     @Override
     public void setup(Window window) {
@@ -38,8 +48,30 @@ public class Main extends Game {
                 .size(window.getWidth(),window.getHeight())
                 .position(window.getWidth()/2,window.getHeight()/2)
                 .build();
-        
+
         renderer2=new Shape2Renderer2();
+
+        map=new LinkedList<>();
+        try{
+            File file=new File("assets/level.txt");
+            Scanner scanner=new Scanner(file);
+
+            int row=0;
+            while(scanner.hasNextLine()){
+                map.add(new LinkedList<>());
+                row++;
+                String line=scanner.nextLine();
+                String[] data=line.split(" ");
+                for(String s:data){
+                    map.get(row-1).add(Integer.parseInt(s));
+                }
+            }
+
+            scanner.close();
+        }catch (Exception e){
+            System.out.println("error reading level file");
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -48,9 +80,38 @@ public class Main extends Game {
         renderer2.view(camera.view());
         renderer2.projection(camera.projection());
         renderer2.texture_$eq(texture);
-        
-        
-        renderer2.draw(new Square(100,100,100,Colors.gray50(),character, new Matrix4f()));
+
+        TextureRegion[] palette=new TextureRegion[]{
+                regions[5][3],
+                regions[2][2],
+                regions[1][2],
+                regions[3][2],
+                regions[2][1],
+        };
+
+        for(int i=0;i<map.get(0).size();i++){
+            for(int j=0;j<map.size();j++){
+                int tileId=map.get(j).get(i);
+                if(tileId==0)continue;
+                renderer2.draw(new Square(
+                        i*tileSize,
+                        j*tileSize,
+                        tileSize,
+                        Colors.white(),
+                        palette[tileId],
+                        new Matrix4f()
+                ));
+            }
+        }
+
+        renderer2.draw(new Square(
+                100,
+                100,
+                tileSize,
+                Colors.gray50(),
+                character,
+                new Matrix4f()
+        ));
         renderer2.flush();
     }
     
