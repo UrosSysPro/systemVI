dac26468224b5e7c5c771e5ccc6381b7558e30f8
Uros Karaleic <karaleicu@gmail.com>
2024-12-10 15:17:12 +0100

    nis

--- examples/cats/src/scala/com/systemvi/cats/Main.scala
+++ examples/cats/src/scala/com/systemvi/cats/Main.scala
@@ -1,20 +1,15 @@
 package com.systemvi.cats
 
-import cats.effect.{IO, IOApp}
-import scala.concurrent.duration._
+import cats.effect.{ExitCode, IO, IOApp}
+
+import scala.concurrent.duration.*
 
 
-object Main extends IOApp.Simple {
-  override def run: IO[Unit] = for {
-    ctr <- IO.ref(0)
+object Main extends IOApp:
 
-    wait = IO.sleep(1.second)
-    poll = wait *> ctr.get
+  override def run(args: List[String]): IO[ExitCode] = program.as(ExitCode.Success)
 
-    _ <- poll.flatMap(IO.println(_)).foreverM.start
-    _ <- poll.map(_ % 3 == 0).ifM(IO.println("fizz"), IO.unit).foreverM.start
-    _ <- poll.map(_ % 5 == 0).ifM(IO.println("buzz"), IO.unit).foreverM.start
+  val program: IO[Unit] = for
+    _ <- IO.println("hello")
+  yield IO.unit
 
-    _ <- (wait *> ctr.update(_ + 1)).foreverM.void
-  } yield ()
-}
