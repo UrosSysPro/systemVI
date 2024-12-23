6fa97dcd0eddb18e95b963491b984fa7f2a6c450
uros <karaleicu@gmail.com>
2024-12-23 23:36:10 +0100

    radi async

--- examples/cats/src/scala/com/systemvi/cats/cats_effect_examples/AsyncExample.scala
+++ examples/cats/src/scala/com/systemvi/cats/cats_effect_examples/AsyncExample.scala
@@ -5,14 +5,23 @@ import cats.*
 import cats.implicits.*
 
 object AsyncExample extends IOApp.Simple {
+
+  extension[A](io:IO[A]){
+    def printThread: IO[A] = io.map{io=>
+      println(s"thread ${Thread.currentThread().getName}")
+      io
+    }
+  }
+
   override def run: IO[Unit] = for {
     a1<-IO.async[String]{cb=>IO{
-
-    }}
-    _<-IO.println(a1)
+      cb("hello".asRight)
+      Some(IO.unit.printThread)
+    }}.printThread
+    _<-IO.println(a1).printThread
     a2<-IO.async_[String]{cb=>
       cb("hello".asRight)
-    }
-    _<-IO.println(a2)
+    }.printThread
+    _<-IO.println(a2).printThread
   } yield ()
 }
