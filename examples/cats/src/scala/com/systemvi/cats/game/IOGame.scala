fc966c970d3cf06de2d643784205ef32348224e8
uros <karaleicu@gmail.com>
2025-01-07 01:13:59 +0100

    radi io game

--- examples/cats/src/scala/com/systemvi/cats/game/IOGame.scala
+++ examples/cats/src/scala/com/systemvi/cats/game/IOGame.scala
@@ -6,16 +6,19 @@ import cats.implicits.*
 
 import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.*
 
 abstract class IOGame {
 
-  extension [A](io:IO[A]){
-    def printThread:IO[A]=io.map {value=>
+  extension [A](io: IO[A]) {
+    def printThread: IO[A] = io.map { value =>
       println(s"${Thread.currentThread().getName} $value")
       value
     }
   }
 
+  var targetFps: Int = 165
+  var targetFrameTime: FiniteDuration =(1d / targetFps).seconds
   var ec: Option[ExecutionContext] = None
 
   def setup: IO[Unit]
@@ -24,10 +27,12 @@ abstract class IOGame {
 
   def dispose: IO[Unit]
 
+  def shouldClose: IO[Boolean]
+
   def run: IO[Unit] = {
 
-    def renderExecutionContextResource=Resource.make[IO,Unit]{
-      IO{
+    def renderExecutionContextResource = Resource.make[IO, Unit] {
+      IO {
         val context = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor((runnable) => {
           val thread = Thread(runnable)
           thread.setDaemon(true)
@@ -36,16 +41,29 @@ abstract class IOGame {
         }))
         ec = context.some
       }
-    }{_=>
+    } { _ =>
       IO.unit
     }
 
-    renderExecutionContextResource.use{_=>
-      val ec=this.ec match
-        case Some(ec)=>ec
+    def gameLoop: IO[Unit] = for {
+      startTime <- IO.realTime
+      _ <- loop
+      endTime <- IO.realTime
+      
+      diff <- IO.pure(endTime - startTime)
+      toSleep <- IO.pure(targetFrameTime - diff)
+      
+      _ <- if toSleep.toNanos > 0 then IO.sleep(toSleep) else IO.unit
+      
+      _ <- shouldClose.ifM(IO.unit, gameLoop)
+    } yield ()
+
+    renderExecutionContextResource.use { _ =>
+      val ec = this.ec match
+        case Some(ec) => ec
       for {
         _ <- setup.evalOn(ec)
-        _ <- loop.evalOn(ec)
+        _ <- gameLoop.evalOn(ec)
         _ <- dispose.evalOn(ec)
       } yield IO.unit
     }
--- examples/cats/src/scala/com/systemvi/cats/game/TestGame.scala
+++ examples/cats/src/scala/com/systemvi/cats/game/TestGame.scala
@@ -6,26 +6,66 @@ import cats.implicits.*
 import cats.*
 import cats.effect.*
 import cats.effect.implicits.*
+import com.systemvi.engine.camera.Camera3
+import com.systemvi.engine.renderers.ShapeRenderer2
+import com.systemvi.engine.ui.utils.data.Colors
+import com.systemvi.engine.utils.Utils
+import com.systemvi.engine.utils.Utils.Buffer
 
-class TestGame extends IOGame{
+class TestGame extends IOGame {
 
-  var window:Option[Window]=None
+  var window: Option[Window] = None
+  var camera: Option[Camera3] = None
+  var polygonRenderer: Option[ShapeRenderer2] = None
 
-  override def setup: IO[Unit] = IO{
-    glfwInit()
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
-    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE)
-    window=Window(800,600,"title").some
-  }
+  given Conversion[Int, Float] = value => value.toFloat
 
-  override def loop: IO[Unit] = IO{
+  override def setup: IO[Unit] = for {
+    _ <- IO {
+      glfwInit()
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+      glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE)
+    }.printThread
+    _ <- IO {
+      window = Window(800, 600, "title").some
+      for (window <- window)
+        camera = Camera3.builder2d()
+          .size(window.getWidth, window.getHeight)
+          .position(window.getWidth / 2, window.getHeight / 2)
+          .build().some
 
-  }
+      polygonRenderer = ShapeRenderer2().some
+      for(camera<-camera;polygonRenderer<-polygonRenderer){
+        polygonRenderer.view(camera.view)
+        polygonRenderer.projection(camera.projection)
+      }
+    }
+  } yield ()
 
-  override def dispose: IO[Unit] = IO{
-    window.foldMap(_.close())
-    glfwTerminate()
+
+  override def loop: IO[Unit] = for {
+    _ <- IO(window.foldMap(_.pollEvents()))
+    _ <- IO(Utils.clear(Colors.black, Buffer.COLOR_BUFFER))
+
+    _<-IO{
+      for(renderer<-polygonRenderer){
+//        renderer.draw(Triang)
+      }
+    }
+
+    _ <- IO(window.foldMap(_.swapBuffers()))
+  } yield ()
+
+  override def dispose: IO[Unit] = for {
+    _ <- IO(window.foldMap(_.close())).printThread
+    _ <- IO(glfwTerminate()).printThread
+  } yield ()
+
+  override def shouldClose: IO[Boolean] = IO {
+    window match
+      case Some(window) => window.shouldClose()
+      case _ => true
   }
 }
