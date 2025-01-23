c9c34385e6626fd6fd91810c42a93d1ee63b2dc2
uros <karaleicu@gmail.com>
2025-01-23 19:04:36 +0100

    firmata real keyboard

--- examples/firmata/src/scala/com/systemvi/firmata/Main.scala
+++ examples/firmata/src/scala/com/systemvi/firmata/Main.scala
@@ -19,33 +19,18 @@ import scala.concurrent.duration.*
 
 object Main extends Game(3,3,60,800,600,"firmata"){
 
-  val col0=2
-  val col1=3
-  val col2=4
-  val row0=5
-  val row1=6
-  val row2=7
-
-//  val col0 = 5
-//  val col1 = 6
-//  val col2 = 7
-//  val row0 = 2
-//  val row1 = 3
-//  val row2 = 4
+  val columns=Array(10,16,14,15,18,19)
+  val rows=Array(9,8,7)
 
   var device:FirmataDevice=null
 
-  var pinCol0:Pin=null
-  var pinCol1:Pin=null
-  var pinCol2:Pin=null
-  var pinRow0:Pin=null
-  var pinRow1:Pin=null
-  var pinRow2:Pin=null
+  var rowPins=Array.empty[Pin]
+  var columnPins=Array.empty[Pin]
 
   var renderer:ShapeRenderer2=null
   var camera:Camera3=null
 
-  val keys:Array[Array[Boolean]]=Array.ofDim(3,3)
+  val keys:Array[Array[Boolean]]=Array.ofDim(rows.length,columns.length)
 
   override def setup(window: Window): Unit = {
 
@@ -74,62 +59,80 @@ object Main extends Game(3,3,60,800,600,"firmata"){
     device.ensureInitializationIsDone()
     println("device ready to use")
 
-    pinCol0 = device.getPin(col0)
-    pinCol1 = device.getPin(col1)
-    pinCol2 = device.getPin(col2)
+    columnPins=columns.map{i=>
+      val pin=device.getPin(i)
+      pin.setMode(Mode.OUTPUT)
+      pin.setValue(0)
+      pin
+    }
+    rowPins=rows.map{i=>
+      val pin=device.getPin(i)
+      pin.setMode(Mode.PULLUP)
+      pin
+    }
+//    pinCol0 = device.getPin(col0)
+//    pinCol1 = device.getPin(col1)
+//    pinCol2 = device.getPin(col2)
 
-    pinCol0.setMode(Mode.OUTPUT)
-    pinCol0.setValue(0)
-    pinCol1.setMode(Mode.OUTPUT)
-    pinCol1.setValue(0)
-    pinCol2.setMode(Mode.OUTPUT)
-    pinCol2.setValue(0)
+//    pinCol0.setMode(Mode.OUTPUT)
+//    pinCol0.setValue(0)
+//    pinCol1.setMode(Mode.OUTPUT)
+//    pinCol1.setValue(0)
+//    pinCol2.setMode(Mode.OUTPUT)
+//    pinCol2.setValue(0)
 
-    pinRow0 = device.getPin(row0)
-    pinRow1 = device.getPin(row1)
-    pinRow2 = device.getPin(row2)
-    pinRow0.setMode(Mode.PULLUP)
-    pinRow1.setMode(Mode.PULLUP)
-    pinRow2.setMode(Mode.PULLUP)
+//    pinRow0 = device.getPin(row0)
+//    pinRow1 = device.getPin(row1)
+//    pinRow2 = device.getPin(row2)
+//    pinRow0.setMode(Mode.PULLUP)
+//    pinRow1.setMode(Mode.PULLUP)
+//    pinRow2.setMode(Mode.PULLUP)
   }
 
   override def loop(delta: Float): Unit = {
-
     Utils.clear(Colors.black)
 
-//    renderer.draw(Square(0,0,50,if pinRow0.getValue==0 then Colors.red400 else Colors.green400))
-//    renderer.draw(Square(50,0,50,if pinRow1.getValue==0 then Colors.red400 else Colors.green400))
-//    renderer.draw(Square(100,0,50,if pinRow2.getValue==0 then Colors.red400 else Colors.green400))
-//    renderer.flush()
+    columnPins.foreach(pin=>pin.setValue(1))
+//    pinCol0.setValue(1)
+//    pinCol1.setValue(1)
+//    pinCol2.setValue(1)
 
-    pinCol0.setValue(1)
-    pinCol1.setValue(1)
-    pinCol2.setValue(1)
-
-    pinCol0.setValue(0)
-    Thread.sleep(1)
-    keys(0)(0)=pinRow0.getValue==1
-    keys(1)(0)=pinRow1.getValue==1
-    keys(2)(0)=pinRow2.getValue==1
-    pinCol0.setValue(1)
-    Thread.sleep(1)
-
-    pinCol1.setValue(0)
-    Thread.sleep(1)
-    keys(0)(1) = pinRow0.getValue == 1
-    keys(1)(1) = pinRow1.getValue == 1
-    keys(2)(1) = pinRow2.getValue == 1
-    pinCol1.setValue(1)
-    Thread.sleep(1)
-
-    pinCol2.setValue(0)
-    Thread.sleep(1)
-    keys(0)(2) = pinRow0.getValue == 1
-    keys(1)(2) = pinRow1.getValue == 1
-    keys(2)(2) = pinRow2.getValue == 1
-    pinCol2.setValue(1)
-    Thread.sleep(1)
-
+    columnPins.zipWithIndex.foreach((columnPin,columnIndex)=>{
+      columnPin.setValue(0)
+      Thread.sleep(1)
+      rowPins.zipWithIndex.foreach((rowPin,rowIndex)=>{
+        keys(rowIndex)(columnIndex)=rowPin.getValue==1
+      })
+//      keys(0)(i) = pinRow0.getValue == 1
+//      keys(1)(0) = pinRow1.getValue == 1
+//      keys(2)(0) = pinRow2.getValue == 1
+      columnPin.setValue(1)
+      Thread.sleep(1)
+    })
+//    pinCol0.setValue(0)
+//    Thread.sleep(1)
+//    keys(0)(0)=pinRow0.getValue==1
+//    keys(1)(0)=pinRow1.getValue==1
+//    keys(2)(0)=pinRow2.getValue==1
+//    pinCol0.setValue(1)
+//    Thread.sleep(1)
+//
+//    pinCol1.setValue(0)
+//    Thread.sleep(1)
+//    keys(0)(1) = pinRow0.getValue == 1
+//    keys(1)(1) = pinRow1.getValue == 1
+//    keys(2)(1) = pinRow2.getValue == 1
+//    pinCol1.setValue(1)
+//    Thread.sleep(1)
+//
+//    pinCol2.setValue(0)
+//    Thread.sleep(1)
+//    keys(0)(2) = pinRow0.getValue == 1
+//    keys(1)(2) = pinRow1.getValue == 1
+//    keys(2)(2) = pinRow2.getValue == 1
+//    pinCol2.setValue(1)
+//    Thread.sleep(1)
+//
     for(i <- 0 until 3){
       for(j <- 0 until 3){
         renderer.draw(Square(i*50f,j*50f,50,if keys(i)(j) then Colors.red400 else Colors.green400))
