2efaeeeb7aec57fa312388aaf75bb607bd3c23af
uros <karaleicu@gmail.com>
2025-03-02 15:57:37 +0100

    keypad test radi

--- arduino/aleksandar_keypad/aleksandar_keypad.ino
+++ arduino/aleksandar_keypad/aleksandar_keypad.ino
@@ -1,22 +1,70 @@
-#include "Arduino.h"
-int rows[]={0,1,2};
-int cols[]={21,20,10,7,6,5,3};
+#include"Arduino.h"
+
+#define COLUMNS_NUMBER 4
+#define ROWS_NUMBER 5
+
+// int columns[]={5,6,7,8,9};
+// int rows[]={0,1,2,3};
+
+int rows[]={5,6,7,8,9};
+int columns[]={0,1,2,3};
+
+struct Key{
+  char value;
+  bool pressed;
+  bool justChanged;
+};
+
+Key keys[COLUMNS_NUMBER][ROWS_NUMBER]={
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+};
 
 void setup() {
-  for(int i = 0; i<3; i++){
-    pinMode(rows[i], OUTPUT);
-    digitalWrite(rows[i], HIGH);
+  for(int i=0;i<COLUMNS_NUMBER;i++){
+    int pin=columns[i];
+    pinMode(pin, INPUT_PULLUP);
   }
-   for(int i = 0; i<7; i++){
-    pinMode(cols[i], PULLUP);
+  for(int i=0;i<ROWS_NUMBER;i++){
+    int pin = rows[i];
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, 1);
   }
 
-// the loop function runs over and over again forever
+  Serial.begin(9600);
+}
+
 void loop() {
-    for(int i = 0; i<3; i++){
-      for(int j = 0; j<7; j++){
-          
+  for(int j=0;j<ROWS_NUMBER;j++){
+    int rowPin=rows[j];
+    digitalWrite(rowPin, 0);
+    delayMicroseconds(1000);
+    for(int i=0;i<COLUMNS_NUMBER;i++){
+      int columnPin=columns[i];
+      bool newState=digitalRead(columnPin) == 0;
+      if(newState!=keys[i][j].pressed)keys[i][j].justChanged=true;
+      keys[i][j].pressed=digitalRead(columnPin) == 0;
+    }
+    digitalWrite(rowPin, 1);
+  }
+
+
+  for(int i=0;i<COLUMNS_NUMBER;i++){
+    for(int j=0;j<ROWS_NUMBER;j++){
+      if(keys[i][j].justChanged){
+        keys[i][j].justChanged=false;
+        if(keys[i][j].pressed){
+          Serial.printf("key pressed %d %d\n",i,j);
+        }else{
+          Serial.printf("key released %d %d\n",i,j);
+        }
       }
     }
- 
+  }
 }
+
+
+
+
