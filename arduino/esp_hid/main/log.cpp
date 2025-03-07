de9c0a4f12d1b3a021a23226b717d2ba43f03f36
Uros Karaleic <karaleicuu@gmail.com>
2025-03-07 14:26:02 +0100

    esp hid example

---
+++ arduino/esp_hid/main/log.cpp
@@ -0,0 +1,10 @@
+#include "log.hpp"
+#include "esp_log.h"
+static char tag[]="DEFAULT_TAG";
+/*void Log::debug(char* message){
+	ESP_LOGI(tag,message);
+}
+void Log::info(char* message){
+	ESP_LOGI(tag,message);
+}
+*/
---
+++ arduino/esp_hid/main/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+
+extern "C"{
+
+	void app_main(void){
+
+	}
+
+}
