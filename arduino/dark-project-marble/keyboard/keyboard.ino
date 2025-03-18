b9f0b5357257fddd35b9607d211122d996028252
uros <karaleicu@gmail.com>
2025-03-19 00:12:52 +0100

    added arrows and f keys to keyboard

--- arduino/dark-project-marble/keyboard/keyboard.ino
+++ arduino/dark-project-marble/keyboard/keyboard.ino
@@ -10,7 +10,7 @@ int rows[]={14,15,16,17,18};
 
 struct Key{
   bool pressed,justChanged;
-  char value;
+  char value,fn;
 };
 
 const byte thisAddress = 8; 
@@ -44,30 +44,46 @@ void setup() {
       if(keys[i][j].justChanged){
         keys[i][j].justChanged=false;
         keys[i][j].pressed=false;
-        keys[i][j].value=' ';
+        keys[i][j].value='\0';
+        keys[i][j].fn='\0';
       }
     }
   }
 
 //red 0///////////////////////////////////////////////////////////////
   keys[13][0].value=KEY_ESC;
+  keys[13][0].fn='`';
   keys[12][0].value='1';
+  keys[12][0].fn=KEY_F1;
   keys[11][0].value='2';
+  keys[11][0].fn=KEY_F2;
   keys[10][0].value='3';
+  keys[10][0].fn=KEY_F3;
   keys[9][0].value='4';
+  keys[9][0].fn=KEY_F4;
   keys[8][0].value='5';
+  keys[8][0].fn=KEY_F5;
   keys[7][0].value='6';
+  keys[7][0].fn=KEY_F6;
   keys[6][0].value='7';
+  keys[6][0].fn=KEY_F7;
   keys[5][0].value='8';
+  keys[5][0].fn=KEY_F8;
   keys[4][0].value='9';
+  keys[4][0].fn=KEY_F9;
   keys[3][0].value='0';
+  keys[3][0].fn=KEY_F10;
   keys[2][0].value='-';
+  keys[2][0].fn=KEY_F11;
   keys[1][0].value='=';
+  keys[1][0].fn=KEY_F12;
   keys[0][0].value=KEY_BACKSPACE;
+  keys[0][0].fn=KEY_DELETE;
 //red 1/////////////////////////////////////////////////////////////
   keys[13][1].value=KEY_TAB;
   keys[12][1].value='q';
   keys[11][1].value='w';
+  keys[11][1].fn=KEY_UP_ARROW;
   keys[10][1].value='e';
   keys[9][1].value='r';
   keys[8][1].value='t';
@@ -82,8 +98,11 @@ void setup() {
 //red 2/////////////////////////////////////////////////////////////
   keys[13][2].value=KEY_CAPS_LOCK;
   keys[12][2].value='a';
+  keys[12][2].fn=KEY_LEFT_ARROW;
   keys[11][2].value='s';
+  keys[11][2].fn=KEY_DOWN_ARROW;
   keys[10][2].value='d';
+  keys[10][2].fn=KEY_RIGHT_ARROW;
   keys[9][2].value='f';
   keys[8][2].value='g';
   keys[7][2].value='h';
@@ -137,6 +156,12 @@ void loop() {
     digitalWrite(rowPin, 1);
   }
 
+  bool fn=false;
+  if(keys[3][4].pressed){
+    keys[3][4].justChanged=false;
+    fn=true;
+  }
+
   for(int i=0;i<COLUMNS_NUMBER;i++){
     for(int j=0;j<ROWS_NUMBER;j++){
       if(keys[i][j].justChanged){
@@ -145,13 +170,21 @@ void loop() {
           #ifdef DEBUG
             Serial.printf("pressed  %3d %3d\n",i,j);
           #else
+          if(fn&&keys[i][j].fn){
+            Keyboard.press(keys[i][j].fn);
+          }else{
             Keyboard.press(keys[i][j].value);
+          }
           #endif
         }else{
           #ifdef DEBUG
             Serial.printf("released %3d %3d\n",i,j);
           #else
+          if(fn&&keys[i][j].fn){
+            Keyboard.release(keys[i][j].fn);
+          }else{
             Keyboard.release(keys[i][j].value);
+          }
           #endif
         } 
       }
