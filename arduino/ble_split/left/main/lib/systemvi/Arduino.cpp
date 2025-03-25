2b6ab809bf2fe514fff7f073e56360cefe8a243c
Uros Karaleic <karaleicuu@gmail.com>
2025-03-25 16:01:59 +0100

    gpio pins in ble split left

--- arduino/ble_split/left/main/lib/systemvi/Arduino.cpp
+++ arduino/ble_split/left/main/lib/systemvi/Arduino.cpp
@@ -1,7 +1,38 @@
 #include"Arduino.hpp"
+#include"driver/gpio.h"
+
+gpio_num_t gpio_pins[]={
+	GPIO_NUM_0,
+	GPIO_NUM_1,
+	GPIO_NUM_2,
+	GPIO_NUM_3,
+	GPIO_NUM_4,
+	GPIO_NUM_5,
+	GPIO_NUM_6,
+	GPIO_NUM_7,
+	GPIO_NUM_8,
+	GPIO_NUM_9,
+	GPIO_NUM_10,
+	GPIO_NUM_11,
+	GPIO_NUM_12,
+	GPIO_NUM_13,
+	GPIO_NUM_14,
+	GPIO_NUM_15,
+	GPIO_NUM_16,
+	GPIO_NUM_17,
+	GPIO_NUM_18,
+	GPIO_NUM_19,
+	GPIO_NUM_20,
+	GPIO_NUM_21,
+};
 
 void pinMode(int pin, pin_mode_t mode){
+	if(mode==INPUT){
+		gpio_set_direction(gpio_pins[pin],GPIO_MODE_INPUT);
+	}
+	if(mode==INPUT_PULLDOWN){
 
+	}
 }
 void digitalWrite(int pin, int value){
 
--- arduino/ble_split/left/main/main.cpp
+++ arduino/ble_split/left/main/main.cpp
@@ -1,222 +1,144 @@
+#include"lib/systemvi/Arduino.hpp"
+#include"lib/systemvi/Keyboard.hpp"
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"freertos/FreeRTOS.h"
-#include"freertos/event_groups.h"
-#include"freertos/task.h"
-#include"esp_flash.h"
-#include"esp_bt.h"
-#include"nvs_flash.h"
+#define COLUMNS_NUMBER 4
+#define ROWS_NUMBER 5
 
-#include"lib/esp_hidd_prf_api.h"
-#include"esp_bt_defs.h"
-#include"esp_gap_ble_api.h"
-#include"esp_bt_main.h"
-#include"lib/hid_dev.h"
+// int columns[]={5,6,7,8,9};
+// int rows[]={0,1,2,3};
 
-bool connected=false;
-int connection_id=0;
+int rows[]={5,6,9,7,8};
+int columns[]={1,0,2,3};
 
+class Key{
+  public:
+  char value;
+  bool pressed;
+  bool justChanged;
+  virtual void onPress(){
+  //  Serial.println("defaultkeypress");
+  	printf("defaultkeypress");
+  }
+  virtual void onRelease(){
+  	printf("defaultkeyrelease");
 
-#define HID_DEMO_TAG "HID_DEMO"
-
-
-#define CHAR_DECLARATION_SIZE   (sizeof(uint8_t))
-
-
-#define HIDD_DEVICE_NAME            "HID"
-static uint8_t hidd_service_uuid128[] = {
-    /* LSB <--------------------------------------------------------------------------------> MSB */
-    //first uuid, 16bit, [12],[13] is the value
-    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x12, 0x18, 0x00, 0x00,
+	  //  Serial.println("defaultkeyrelease");
+  }
 };
 
-esp_ble_adv_data_t hidd_adv_data={
-	.set_scan_rsp=false,
-	.include_name=true,
-	.include_txpower=true,
-	.min_interval=0x0006,
-	.max_interval=0x0010,
-	.appearance=0x03c0,
-	.manufacturer_len=0,
-	.p_manufacturer_data=NULL,
-	.service_data_len=0,
-	.p_service_data=NULL,
-	.service_uuid_len=sizeof(hidd_service_uuid128),
-	.p_service_uuid=hidd_service_uuid128,
-	.flag=0x6,
+class CopyKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("copy");
+  }
 };
-esp_ble_adv_params_t hidd_adv_params={
-	.adv_int_min=0x20,
-	.adv_int_max=0x30,
-	.adv_type=ADV_TYPE_IND,
-	.own_addr_type=BLE_ADDR_TYPE_PUBLIC,
-	.channel_map=ADV_CHNL_ALL,
-	.adv_filter_policy=ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+
+class PasteKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("paste");
+  }
 };
-void hidd_callback(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param){
-	switch(event){
-		case ESP_HIDD_EVENT_REG_FINISH:{
-			if(param->init_finish.state==ESP_HIDD_INIT_OK){
-				esp_ble_gap_set_device_name(HIDD_DEVICE_NAME);
-				esp_ble_gap_config_adv_data(&hidd_adv_data);
-			}
-		}break;
-		case ESP_BAT_EVENT_REG:{}break;
-		case ESP_HIDD_EVENT_DEINIT_FINISH:{}break;
-		case ESP_HIDD_EVENT_BLE_CONNECT:{
-			printf("esp hidd event ble connect\n");
-			connection_id=param->connect.conn_id;
-		}break;
-		case ESP_HIDD_EVENT_BLE_DISCONNECT:{
-			connected=false;
-			printf("esp hidd event ble disconnect");
-		}break;
-		case ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT:{
-			printf("esp hidd event ble vendor report write event");
-		}break;
-		case ESP_HIDD_EVENT_BLE_LED_REPORT_WRITE_EVT:{
-			printf("esp hidd event ble led report write event");
-		}break;
-		default:{} break;
-	}
-}
-void gap_callback(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param){
-	switch(event){
-		case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
-			esp_ble_gap_start_advertising(&hidd_adv_params);
-			break;
-		case ESP_GAP_BLE_SEC_REQ_EVT:
-			for(int i=0;i<ESP_BD_ADDR_LEN;i++){
-				printf("%x",param->ble_security.ble_req.bd_addr[i]);
-			}
-			printf("\n");
-			esp_ble_gap_security_rsp(param->ble_security.ble_req.bd_addr,true);
-			break;
-		case ESP_GAP_BLE_AUTH_CMPL_EVT:
-			connected=true;
-			esp_bd_addr_t bd_addr;
-			memcpy(bd_addr,param->ble_security.auth_cmpl.bd_addr, sizeof(esp_bd_addr_t));
-			printf("remote BD_ADDR: %08x %04x",
-				(bd_addr[0]<<24)+(bd_addr[1]<<16)+(bd_addr[2]<< 8)+(bd_addr[3]<< 0),
-				(bd_addr[4]<< 8)+(bd_addr[5]<< 0)
-			);
-			printf("addres type %d",param->ble_security.auth_cmpl.addr_type);
-			printf("pair status %s",param->ble_security.auth_cmpl.success?"success":"fail");
-			if(!param->ble_security.auth_cmpl.success){
-				printf("fail reason 0x%x",param->ble_security.auth_cmpl.fail_reason);
-			}
-			break;
-		default:
-			break;
-	}
+class UndoKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("undo");
+  }
+};
+class RedoKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("Redo");
+  }
+};
+class BSIKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("BSI");
+  }
+};
+class BSDKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("BSD");
+  }
+};
+class ZOOMKey:public Key{
+  public:
+  void onPress()override{
+  //  Serial.println("zoom");
+  }
+};
+
+//Key keys[COLUMNS_NUMBER][ROWS_NUMBER];
+/*={
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+  {{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false},{' ',false,false}},
+};*/
+
+Key ***keys;
+
+void setup() {
+
+  keys=new Key**[COLUMNS_NUMBER];
+  for(int i=0;i<COLUMNS_NUMBER;i++){
+    keys[i]=new Key*[ROWS_NUMBER];
+    for(int j=0;j<ROWS_NUMBER;j++){
+      if(i==0 && j==2){
+        keys[i][j]=new CopyKey();
+      
+      }else{
+        keys[i][j]=new Key();
+      }
+    }
+  }
+
+  for(int i=0;i<COLUMNS_NUMBER;i++){
+    int pin=columns[i];
+    pinMode(pin, INPUT_PULLUP);
+  }
+  for(int i=0;i<ROWS_NUMBER;i++){
+    int pin = rows[i];
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, 1);
+  }
+
+//  Serial.begin(9600);
 }
 
-bool KeyboardisConnected(){
-	return connected;
+void loop() {
+  for(int j=0;j<ROWS_NUMBER;j++){
+    int rowPin=rows[j];
+    digitalWrite(rowPin, 0);
+    delayMicroseconds(1000);
+    for(int i=0;i<COLUMNS_NUMBER;i++){
+      int columnPin=columns[i];
+      bool newState=digitalRead(columnPin) == 0;
+      if(newState!=keys[i][j]->pressed)keys[i][j]->justChanged=true;
+      keys[i][j]->pressed=digitalRead(columnPin) == 0;
+    }
+    digitalWrite(rowPin, 1);
+  }
+
+
+  for(int i=0;i<COLUMNS_NUMBER;i++){
+    for(int j=0;j<ROWS_NUMBER;j++){
+      if(keys[i][j]->justChanged){
+        keys[i][j]->justChanged=false;
+        if(keys[i][j]->pressed){
+          keys[i][j]->onPress();
+        //  Serial.printf("key pressed %d %d\n",i,j);
+        }else{
+          keys[i][j]->onRelease();
+        //  Serial.printf("key released %d %d\n",i,j);
+        }
+      }
+    }
+  }
 }
 
-void Keyboardpress(uint8_t key){
-	esp_hidd_send_keyboard_value(connection_id,0,&key,1);
-}
 
-void KeyboardRelease(uint8_t key){
-	esp_hidd_send_keyboard_value(connection_id,0,&key,0);
-}
-void delay(int millis){
-	vTaskDelay(millis/portTICK_PERIOD_MS);
-}
-void task_main(void *params){
-	while(1){
-		delay(3000);
-		if(KeyboardisConnected()){
-			printf("send key press\n");
-			uint8_t key=HID_KEY_D;
-			Keyboardpress(key);
-			delay(100);
-			KeyboardRelease(key);
-		}
-	}
-}
 
-void initFlash(){
-	esp_err_t ret;
-		
-	ret=nvs_flash_init();
-	if(ret==ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND){
-		nvs_flash_erase();
-		ret=nvs_flash_init();
-	}
-}
-void enableBleController(){
-	esp_err_t ret;
-	esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
-	esp_bt_controller_config_t bt_config=BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-	ret=esp_bt_controller_init(&bt_config);
-	if(ret){
-		printf("initialize controller failed\n");
-		exit(1);
-	}	
-	ret=esp_bt_controller_enable(ESP_BT_MODE_BLE);
-	if(ret){
-		printf("enable ble controller failed\n");
-		exit(1);
-	}
-}
-void enableBluedroid(){
-	esp_err_t ret;
-	ret=esp_bluedroid_init();
-	if(ret){
-		printf("init bluedroid failed\n");
-		exit(1);
-	}
-	ret=esp_bluedroid_enable();
-	if(ret){
-		printf("enable bluedroid failedi\n");
-		exit(1);
-	}	
-}
-void enableHidProfile(){
-	esp_err_t ret;
-	ret=esp_hidd_profile_init();
-	if(ret!=ESP_OK){
-		printf("init hidd profile failed\n");
-		exit(1);
-	}
-}
-void setBleHidCallbacks(){
-	esp_ble_gap_register_callback(gap_callback);
-	esp_hidd_register_callbacks(hidd_callback);
-}
-void enableBleSecurity(){
-	esp_ble_auth_req_t auth_req=ESP_LE_AUTH_BOND;
-	esp_ble_io_cap_t iocap=ESP_IO_CAP_NONE;
-	uint8_t key_size=16;
-	uint8_t init_key = ESP_BLE_ENC_KEY_MASK|ESP_BLE_ID_KEY_MASK;
-	uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK|ESP_BLE_ID_KEY_MASK;
-	esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE,&auth_req,sizeof(uint8_t));
-	esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE,&iocap,sizeof(uint8_t));
-	esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE,&key_size,sizeof(uint8_t));
-	esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE,&init_key,sizeof(uint8_t));
-	esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE,&rsp_key,sizeof(uint8_t));
-}
 
-void KeyboardInit(){
-	initFlash();
-	enableBleController();
-	enableBluedroid();
-	enableHidProfile();
-	setBleHidCallbacks();
-	enableBleSecurity();
-}
-void init(){
-	
-}
-
-void loop(){
-
-}
-extern "C" void app_main(void){
-	KeyboardInit();
-	xTaskCreate(&task_main,"hid_task", 2048, NULL, 5, NULL);
-}
