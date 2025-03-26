fcf1902998bb8bfe712dea5f0cbdc9ab55556b4a
Uros Karaleic <karaleicuu@gmail.com>
2025-03-26 03:18:33 +0100

    bluetooth api

--- arduino/ble_split/left/main/lib/systemvi/Bluetooth.cpp
+++ arduino/ble_split/left/main/lib/systemvi/Bluetooth.cpp
@@ -1,1 +1,110 @@
 #include"Bluetooth.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+
+#define GATTS_TABLE_TAG "GATTS_TABLE_DEMO"
+
+#define PROFILE_NUM                 1
+#define PROFILE_APP_IDX             0
+#define ESP_APP_ID                  0x55
+#define SAMPLE_DEVICE_NAME          "ESP_GATTS_DEMO"
+#define SVC_INST_ID                 0
+
+/* The max length of characteristic value. When the GATT client performs a write or prepare write operation,
+*  the data length must be less than GATTS_DEMO_CHAR_VAL_LEN_MAX.
+*/
+#define GATTS_DEMO_CHAR_VAL_LEN_MAX 500
+#define PREPARE_BUF_MAX_SIZE        1024
+#define CHAR_DECLARATION_SIZE       (sizeof(uint8_t))
+
+#define ADV_CONFIG_FLAG             (1 << 0)
+#define SCAN_RSP_CONFIG_FLAG        (1 << 1)
+void initFlash(){
+	esp_err_t ret;
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+}
+
+void enableBle(){
+	esp_err_t ret;
+	ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ret = esp_bt_controller_init(&bt_cfg);
+    if (ret) {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    if (ret) {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+}
+
+void enableBluedroid(){
+	esp_err_t ret;
+	ret = esp_bluedroid_init();
+    if (ret) {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s init bluetooth failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    ret = esp_bluedroid_enable();
+    if (ret) {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s enable bluetooth failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+}
+
+void Bluetooth::init(){
+	initFlash();	
+	enableBle();
+	enableBluedroid();
+}
+
+void registerProfile(int app_id,esp_gatts_cb_t gattsCallback,esp_gap_ble_cb_t gapCallback,int mtu){
+	esp_err_t ret = esp_ble_gatts_register_callback(gattsCallback);
+    if (ret){
+        ESP_LOGE(GATTS_TABLE_TAG, "gatts register error, error code = %x", ret);
+        return;
+    }
+
+    ret = esp_ble_gap_register_callback(gapCallback);
+    if (ret){
+        ESP_LOGE(GATTS_TABLE_TAG, "gap register error, error code = %x", ret);
+        return;
+    }
+
+    ret = esp_ble_gatts_app_register(app_id);
+    if (ret){
+        ESP_LOGE(GATTS_TABLE_TAG, "gatts app register error, error code = %x", ret);
+        return;
+    }
+
+    esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(mtu);
+    if (local_mtu_ret){
+        ESP_LOGE(GATTS_TABLE_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
+    }
+
+}
+
+
+BluetoothProfile Bluetooth::createProfile(BluetoothProfileConfig config){
+	registerProfile(config.id,config.gattsCallback,config.gapCallback,config.mtu);
+	return BluetoothProfile();
+}
--- arduino/ble_split/left/main/lib/systemvi/Bluetooth.hpp
+++ arduino/ble_split/left/main/lib/systemvi/Bluetooth.hpp
@@ -1,4 +1,31 @@
 #ifndef __BLUETOOTH__
 #define __BLUETOOTH__
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
 
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+class BluetoothProfile{
+
+};
+
+class BluetoothProfileConfig{
+	public:
+	int id,mtu;
+	esp_gatts_cb_t gattsCallback;
+	esp_gap_ble_cb_t gapCallback;
+};
+
+class Bluetooth{
+	public:
+	static BluetoothProfile createProfile(BluetoothProfileConfig config);
+	static void init();
+};
 #endif
