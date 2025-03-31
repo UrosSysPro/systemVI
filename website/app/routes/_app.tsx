76a8bf0ac647d01c83329aeae144966d07c7c9d5
uros <karaleicu@gmail.com>
2025-04-01 00:01:39 +0200

    laminar website start

--- website/app/routes/_app.tsx
+++
@@ -1,9 +0,0 @@
-import {Outlet} from "@remix-run/react";
-import {Header} from "~/components/app/Header";
-
-export default function (){
-    return <div className={"overflow-x-hidden overflow-y-auto overscroll-none scrollbar-hide pt-14"}>
-        <Header/>
-        <Outlet/>
-    </div>
-}
