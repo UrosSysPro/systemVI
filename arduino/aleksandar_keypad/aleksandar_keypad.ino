1ee17cd098076dd087a9ddb0cd5910f17dbedb49
Uros Karaleic <karaleicuu@gmail.com>
2025-04-06 18:37:53 +0200

    aleksandar keypad radi jej

--- arduino/aleksandar_keypad/aleksandar_keypad.ino
+++ arduino/aleksandar_keypad/aleksandar_keypad.ino
@@ -1,13 +1,13 @@
 #include"Arduino.h"
-
+#include"Keyboard.h"
 #define COLUMNS_NUMBER 4
-#define ROWS_NUMBER 
+#define ROWS_NUMBER 5
 
 // int columns[]={5,6,7,8,9};
 // int rows[]={0,1,2,3};
 
-int rows[]={5,6,9,7,8};
-int columns[]={1,0,2,3};
+int rows[]={2,3,4,5,6};
+int columns[]={10,16,14,15};
 
 class Key{
   public:
@@ -19,6 +19,7 @@ class Key{
   }
   virtual void onRelease(){
     Serial.println("defaultkeyrelease");
+    Keyboard.releaseAll();
   }
 };
 
@@ -26,6 +27,10 @@ class CopyKey:public Key{
   public:
   void onPress()override{
     Serial.println("copy");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('c');
+    delay(100);
+    Keyboard.releaseAll();
   }
 };
 
@@ -33,36 +38,65 @@ class PasteKey:public Key{
   public:
   void onPress()override{
     Serial.println("paste");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('v');
+    delay(100);
+    Keyboard.releaseAll();
   }
 };
 class UndoKey:public Key{
   public:
   void onPress()override{
     Serial.println("undo");
+        Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('z');
+    delay(100);
+    Keyboard.releaseAll();
   }
 };
 class RedoKey:public Key{
   public:
   void onPress()override{
     Serial.println("Redo");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press('z');
+    delay(100);
+    Keyboard.releaseAll();
   }
 };
 class BSIKey:public Key{
   public:
   void onPress()override{
     Serial.println("BSI");
+    Keyboard.press(']');
   }
 };
 class BSDKey:public Key{
   public:
   void onPress()override{
     Serial.println("BSD");
+    Keyboard.press('[');
   }
 };
 class ZOOMKey:public Key{
   public:
   void onPress()override{
     Serial.println("zoom");
+    Keyboard.press('z');
+  }
+};
+class BrushKey:public Key{
+  public:
+  void onPress()override{
+    Serial.println("brush");
+  }
+};
+class EraserKey:public Key{
+  public:
+  void onPress()override{
+    Serial.println("eraser");
+
   }
 };
 
@@ -82,10 +116,34 @@ void setup() {
   for(int i=0;i<COLUMNS_NUMBER;i++){
     keys[i]=new Key*[ROWS_NUMBER];
     for(int j=0;j<ROWS_NUMBER;j++){
-      if(i==0 && j==2){
+      if(i==0 && j==3){
         keys[i][j]=new CopyKey();
-      
-      }else{
+        }
+      else if(i==3 && j ==1){
+        keys[i][j]=new BSIKey();
+      }
+      else if(i==3 && j ==2){
+        keys[i][j]=new BSDKey();
+      }
+      else if(i==1 && j ==3){
+        keys[i][j]=new PasteKey();
+      }
+      else if(i==0 && j ==1){
+        keys[i][j]=new UndoKey();
+      }
+      else if(i==1 && j ==1){
+        keys[i][j]=new RedoKey();
+      }
+      else if(i==0 && j ==2){
+        keys[i][j]=new BrushKey();
+      }
+      else if(i==1 && j ==2){
+        keys[i][j]=new EraserKey();
+      }
+      else if(i==3 && j==4){
+        keys[i][j]=new ZOOMKey();
+      }
+      else{
         keys[i][j]=new Key();
       }
     }
@@ -124,8 +182,13 @@ void loop() {
       if(keys[i][j]->justChanged){
         keys[i][j]->justChanged=false;
         if(keys[i][j]->pressed){
+
           keys[i][j]->onPress();
-        //  Serial.printf("key pressed %d %d\n",i,j);
+        //Serial.println("undo");
+        // Serial.println("key pressed");
+        //Serial.print(columns[i]);
+        //Serial.print(rows[j]);
+        //Serial.println(" ");
         }else{
           keys[i][j]->onRelease();
         //  Serial.printf("key released %d %d\n",i,j);
