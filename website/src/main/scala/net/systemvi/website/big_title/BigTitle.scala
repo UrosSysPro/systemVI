f8413430e09991dcd3cc3e1dd11a8a8666540ccb
uros <karaleicu@gmail.com>
2025-05-31 00:34:12 +0200

    keyboard page

--- website/src/main/scala/net/systemvi/website/big_title/BigTitle.scala
+++ website/src/main/scala/net/systemvi/website/big_title/BigTitle.scala
@@ -1,5 +1,14 @@
 package net.systemvi.website.big_title
 
-class BigTitle {
+import com.raquo.laminar.api.L.{*, given}
+import org.scalajs.dom
 
+def BigTitle(title:String):Element={
+  h1(
+    padding.rem:=1,
+    fontSize.rem:=6,
+    color:="#22272d",
+    fontWeight.bold,
+    title
+  )
 }
--- website/src/main/scala/net/systemvi/website/views/KeyboardsPageView.scala
+++ website/src/main/scala/net/systemvi/website/views/KeyboardsPageView.scala
@@ -1,6 +1,9 @@
 package net.systemvi.website.views
 
 import com.raquo.laminar.api.L.{*, given}
+import net.systemvi.website.KeyboardPage
+import net.systemvi.website.api.KeyboardApi
+import net.systemvi.website.big_title.BigTitle
 import net.systemvi.website.footer.Footer
 import net.systemvi.website.navbar.Navbar
 import net.systemvi.website.section.{Section, SectionItem}
@@ -8,12 +11,17 @@ import net.systemvi.website.slider.ImageSlider
 import org.scalajs.dom
 
 def KeyboardsPageView():Element = {
+  val keyboards=KeyboardApi.all()
   div(
     cls:="flex flex-col items-center pt-24",
     div(
       className:="flex flex-col justify-start w-full max-w-[1450px]",
       Navbar(),
-
+      BigTitle("Keyboards"),
+      Section(
+        title = "",
+        items = keyboards.map(k=>SectionItem(k.name,k.images.head,KeyboardPage(k.id)))
+      ),
       Footer(),
     )
   )
