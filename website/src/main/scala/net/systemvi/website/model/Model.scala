5c814b80423c75775707cefc86be99a69d2f4c8a
uros <karaleicu@gmail.com>
2025-05-31 01:23:03 +0200

    engine page

--- website/src/main/scala/net/systemvi/website/model/Model.scala
+++ website/src/main/scala/net/systemvi/website/model/Model.scala
@@ -5,4 +5,5 @@ case class Keyboard(id:Int,name:String, codeName:String, specs:List[KeyboardSpec
 
 case class Game(id:Int,name:String,codeName:String,images:List[String])
 
-case class Engine()
+case class Engine(id:Int,version:String,name:String,codeName:String,description:String,demos:List[EngineDemo])
+case class EngineDemo(id:Int,name:String,description:String,images:List[String])
--- website/src/main/scala/net/systemvi/website/views/EnginePageView.scala
+++ website/src/main/scala/net/systemvi/website/views/EnginePageView.scala
@@ -1,42 +1,29 @@
 package net.systemvi.website.views
 
 import com.raquo.laminar.api.L.{*, given}
+import net.systemvi.website.HomePage
+import net.systemvi.website.api.EngineApi
 import net.systemvi.website.footer.Footer
 import net.systemvi.website.navbar.Navbar
 import net.systemvi.website.section.{Section, SectionItem}
 import net.systemvi.website.slider.ImageSlider
+import net.systemvi.website.big_title.BigTitle
+import net.systemvi.website.model.*
 import org.scalajs.dom
 
 def EnginePageView():Element = {
+  val engine=EngineApi.get()
   div(
     cls:="flex flex-col items-center pt-24",
     div(
       className:="flex flex-col justify-start w-full max-w-[1450px]",
       Navbar(),
-//      ImageSlider(
-//        images = List(
-//          "images/keyboards-all.jpg",
-//          "images/keyboards-all-rotated.jpg",
-//        )
-//      ),
-//      Section("Builds",List(
-//        SectionItem("Corne Wireless","images/corne-wireless.jpg"),
-//        SectionItem("Corne Prototype","images/corne-prototype.jpg"),
-//        SectionItem("PH Design 60%","images/keyboard-60.jpg"),
-//        SectionItem("Bana 40%","images/red-keyboard.jpg"),
-//      )),
-//      Section("In Progress",List(
-//        SectionItem("TKL Rabbit","images/tkl-rabbit.jpg"),
-//        SectionItem("7x5 Dactyl","images/dactyl2.jpg"),
-////        SectionItem("Modded YENKEE","item2.png"),
-//      )),
-//      Section("Tools",List(
-//        SectionItem("Soldering Iron","item1.png"),
-//        SectionItem("Pliers","item1.png"),
-//        SectionItem("Key Switch","item1.png"),
-//        SectionItem("Key Caps","item1.png"),
-//      )),
-      //  AboutSection(),
+      BigTitle("Engine"),
+      ImageSlider(engine.demos.flatMap(d=>d.images)),
+      Section(
+        title = "",
+        items = engine.demos.map(d=>SectionItem(d.name,d.images.head,HomePage))
+      ),
       Footer(),
     )
   )
