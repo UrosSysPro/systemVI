05d729ce5d04db7d6bc01f473ec71222df990878
uros <karaleicu@gmail.com>
2025-06-02 05:26:10 +0200

    bill of materials, secion title, view all button

--- website/src/main/scala/net/systemvi/website/bill_of_materials/BillOfMaterials.scala
+++ website/src/main/scala/net/systemvi/website/bill_of_materials/BillOfMaterials.scala
@@ -12,26 +12,36 @@ def BillOfMaterials():HtmlElement ={
     div(
       flex:="1",
       position.relative,
-      display.flex,
-      flexDirection.column,
-      overflowY.auto,
-      hiddenScrollbar,
-      gap:="10px",
-      List.range(0,20).map(_=>div(
-        div(
-          paddingLeft.rem:=2,
-          paddingRight.rem:=2,
-          paddingTop.rem:=1,
-          paddingBottom.rem:=1,
-          backgroundColor.rgb(246,246,246),
-          borderRadius.rem:=1,
-          color.gray,
-          fontSize.rem:=2,
-          fontWeight:="500",
-          "Asdfsdf Asdf SDF GSDG"
-        )
-      )),
-
+      div(
+        display.flex,
+        flexDirection.column,
+        overflowY.auto,
+        hiddenScrollbar,
+        gap:="10px",
+        height.percent:=100,
+        List.range(0,20).map(_=>div(
+          div(
+            paddingLeft.rem:=2,
+            paddingRight.rem:=2,
+            paddingTop.rem:=1,
+            paddingBottom.rem:=1,
+            backgroundColor.rgb(246,246,246),
+            borderRadius.rem:=1,
+            color.gray,
+            fontSize.rem:=2,
+            fontWeight:="500",
+            "Asdfsdf Asdf SDF GSDG"
+          )
+        )),
+      ),
+      div(
+        position.absolute,
+        bottom.rem:=0,
+        left.rem:=0,
+        right.rem:=0,
+        height.rem:=4,
+        background:="linear-gradient(rgba(255,255,255,0), #fff)"
+      )
     ),
     div(
       flex:="1",
--- website/src/main/scala/net/systemvi/website/section/SectionTitle.scala
+++ website/src/main/scala/net/systemvi/website/section/SectionTitle.scala
@@ -13,15 +13,22 @@ def SectionTitle(text:String,viewAllPage:Page=null):HtmlElement=div(
     fontSize:="2rem",
     text
   ),
-  if viewAllPage!=null then a(
-    router.navigateTo(viewAllPage),
-    borderRadius:="0.3rem",
-    backgroundColor:="#eee",
-    paddingLeft.rem:=2,
-    paddingRight.rem:=2,
-    paddingTop:="0.5rem",
-    paddingBottom:="0.5rem",
-    "View All ->"
-  ) else emptyNode
+  if viewAllPage!=null then div(
+    display.flex,
+    justifyContent.center,
+    alignItems.center,
+    a(
+      router.navigateTo(viewAllPage),
+      borderRadius:="0.3rem",
+      backgroundColor:="#eee",
+      transition:="300ms",
+      paddingLeft.rem:=2,
+      paddingRight.rem:=2,
+      paddingTop:="0.5rem",
+      paddingBottom:="0.5rem",
+      "View All ->"
+    )
+  )
+  else emptyNode
 )
 
