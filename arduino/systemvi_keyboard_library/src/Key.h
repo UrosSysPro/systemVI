feab136eb00671d06fda0c8645bacb6b713c9517
Uros Karaleic <karaleicuu@gmail.com>
2025-07-07 22:43:44 +0200

    keys

--- arduino/systemvi_keyboard_library/src/Key.h
+++ arduino/systemvi_keyboard_library/src/Key.h
@@ -1,6 +1,32 @@
 #ifndef KEY_H
 #define KEY_H
 
+#include "Arduino.h"
+// #include ""
 
+class Key {
+public:
+    Key();
+    bool pressed,justChanged,active;
+    int width,height,matrixX,matrixY;
+    virtual void onPress();
+    virtual void onRelease();
+};
 
+class NormalKey : public Key {
+public:
+    NormalKey();
+    char value[4],currentlyDown;
+};
+
+class MacroAction {
+public:
+    MacroAction();
+    int type;
+    char value;
+};
+
+class MacroKey : public Key {
+
+};
 #endif
