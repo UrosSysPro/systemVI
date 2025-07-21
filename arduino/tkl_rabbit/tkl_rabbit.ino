971e6fabd1d4d77994f18770986407b0ad242ba3
Uros Karaleic <karaleicuu@gmail.com>
2025-07-21 23:38:13 +0200

    tkl rabbit skoro skroz radi firmware

--- arduino/tkl_rabbit/tkl_rabbit.ino
+++ arduino/tkl_rabbit/tkl_rabbit.ino
@@ -1,4 +1,5 @@
 #include "SystemVIKeyboard.h"
+#include "Keyboard.h"
 
 #define COLUMNS_NUMBER 17
 #define ROWS_NUMBER 6
@@ -82,62 +83,59 @@ void setup() {
   keyboard->setNormalKeycap( 6,2,  (byte[]){'p',                '\0','\0','\0'},     10,2,      0,0);
   keyboard->setNormalKeycap( 5,2,  (byte[]){'[',                '\0','\0','\0'},     11,2,      0,0);
   keyboard->setNormalKeycap( 4,2,  (byte[]){']',                '\0','\0','\0'},     12,2,      0,0);
-  
+
   keyboard->setNormalKeycap( 2,2,  (byte[]){KEY_DELETE,         '\0','\0','\0'},     14,2,      0,0);
   keyboard->setNormalKeycap( 1,2,  (byte[]){KEY_END,            '\0','\0','\0'},     15,2,      0,0);
   keyboard->setNormalKeycap( 0,2,  (byte[]){KEY_PAGE_DOWN,      '\0','\0','\0'},     16,2,      0,0);
 // //red 3/////////////////////////////////////////////////////////////
-  keys[16][3].setLayer0SizePosition(KEY_CAPS_LOCK,3,0,0,3);
-  keys[15][3].setLayer0Position('a', 1,3);
-  keys[14][3].setLayer0Position('s', 2,3);
-  keys[13][3].setLayer0Position('d', 3,3);
-  keys[12][3].setLayer0Position('f', 4,3);
-  keys[11][3].setLayer0Position('g', 5,3);
-  keys[10][3].setLayer0Position('h', 6,3);
-  keys[9][3].setLayer0Position('j', 7,3);
-  keys[8][3].setLayer0Position('k', 8,3);
-  keys[7][3].setLayer0Position('l', 9,3);
-  keys[6][3].setLayer0Position(';', 10,3);
-  keys[5][3].setLayer0Position('\'', 11,3);
-  keys[4][3].setLayer0SizePosition('\\',2,0, 13,2);
-  keys[3][3].setLayer0SizePosition('\n',5,0, 12,3);
+  keyboard->setNormalKeycap(16,3,  (byte[]){KEY_CAPS_LOCK,      '\0','\0','\0'},      3,0,      0,0);
+  keyboard->setNormalKeycap(15,3,  (byte[]){'a',                '\0','\0','\0'},      1,3,      0,0);
+  keyboard->setNormalKeycap(14,3,  (byte[]){'s',                '\0','\0','\0'},      2,3,      0,0);
+  keyboard->setNormalKeycap(13,3,  (byte[]){'d',                '\0','\0','\0'},      3,3,      0,0);
+  keyboard->setNormalKeycap(12,3,  (byte[]){'f',                '\0','\0','\0'},      4,3,      0,0);
+  keyboard->setNormalKeycap(11,3,  (byte[]){'g',                '\0','\0','\0'},      5,3,      0,0);
+  keyboard->setNormalKeycap(10,3,  (byte[]){'h',                '\0','\0','\0'},      6,3,      0,0);
+  keyboard->setNormalKeycap( 9,3,  (byte[]){'j',                '\0','\0','\0'},      7,3,      0,0);
+  keyboard->setNormalKeycap( 8,3,  (byte[]){'k',                '\0','\0','\0'},      8,3,      0,0);
+  keyboard->setNormalKeycap( 7,3,  (byte[]){'l',                '\0','\0','\0'},      9,3,      0,0);
+  keyboard->setNormalKeycap( 6,3,  (byte[]){';',                '\0','\0','\0'},     10,3,      0,0);
+  keyboard->setNormalKeycap( 5,3,  (byte[]){'\'',               '\0','\0','\0'},     11,3,      0,0);
+  keyboard->setNormalKeycap( 4,3,  (byte[]){'\\',               '\0','\0','\0'},      2,0,      0,0);
+  keyboard->setNormalKeycap( 3,3,  (byte[]){'\n',               '\0','\0','\0'},      5,0,      0,0);
 
-  keys[2][3].setLayer0Position(KEY_DELETE, 13,3);
-  keys[1][3].setLayer0Position(KEY_END, 14,3);
-  keys[0][3].setLayer0Position(KEY_PAGE_DOWN, 15,3);
+  keyboard->setNormalKeycap( 2,3,  (byte[]){KEY_DELETE,         '\0'.'\0'.'\0'},      13,3,     0,0);
+  keyboard->setNormalKeycap( 1,3,  (byte[]){KEY_END,            '\0'.'\0'.'\0'},      14,3,     0,0);
+  keyboard->setNormalKeycap( 0,3,  (byte[]){KEY_PAGE_DOWN,      '\0'.'\0'.'\0'},      15,3,     0,0);
 // //red 4/////////////////////////////////////////////////////////////
-  keys[16][4].setLayer0SizePosition(KEY_LEFT_SHIFT,1,0, 0,4);
-  keys[15][4].setLayer0Position('/', 1,4); 
-  keys[14][4].setLayer0Position('z', 2,4);
-  keys[13][4].setLayer0Position('x', 3,4); 
-  keys[12][4].setLayer0Position('c', 4,4); 
-  keys[11][4].setLayer0Position('v', 5,4); 
-  keys[10][4].setLayer0Position('b', 6,4); 
-  keys[9][4].setLayer0Position('n', 7,4); 
-  keys[8][4].setLayer0Position('m', 8,4); 
-  keys[7][4].setLayer0Position(',', 9,4); 
-  keys[6][4].setLayer0Position('.', 10,4); 
-  keys[5][4].setLayer0Position('/', 11,4); 
-  keys[4][4].setLayer0SizePosition(KEY_RIGHT_SHIFT,6,0, 12,4); 
-  keys[1][4].setLayer0Position(KEY_UP_ARROW, 13,4); 
+  keyboard->setNormalKeycap(16,4,  (byte[]){KEY_LEFT_SHIFT,     '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap(15,4,  (byte[]){'/',                '\0','\0','\0'},       1,4,     0,0); 
+  keyboard->setNormalKeycap(14,4,  (byte[]){'z',                '\0','\0','\0'},       2,4,     0,0);
+  keyboard->setNormalKeycap(13,4,  (byte[]){'x',                '\0','\0','\0'},       3,4,     0,0); 
+  keyboard->setNormalKeycap(12,4,  (byte[]){'c',                '\0','\0','\0'},       4,4,     0,0); 
+  keyboard->setNormalKeycap(11,4,  (byte[]){'v',                '\0','\0','\0'},       5,4,     0,0); 
+  keyboard->setNormalKeycap(10,4,  (byte[]){'b',                '\0','\0','\0'},       6,4,     0,0); 
+  keyboard->setNormalKeycap( 9,4,  (byte[]){'n',                '\0','\0','\0'},       7,4,     0,0); 
+  keyboard->setNormalKeycap( 8,4,  (byte[]){'m',                '\0','\0','\0'},       8,4,     0,0); 
+  keyboard->setNormalKeycap( 7,4,  (byte[]){',',                '\0','\0','\0'},       9,4,     0,0); 
+  keyboard->setNormalKeycap( 6,4,  (byte[]){'.',                '\0','\0','\0'},      10,4,     0,0); 
+  keyboard->setNormalKeycap( 5,4,  (byte[]){'/',                '\0','\0','\0'},      11,4,     0,0); 
+  keyboard->setNormalKeycap( 4,4,  (byte[]){KEY_RIGHT_SHIFT,    '\0','\0','\0'},       6,0,     0,0); 
+  keyboard->setNormalKeycap( 1,4,  (byte[]){KEY_UP_ARROW,       '\0','\0','\0'},      13,4,     0,0); 
 
 // //red 5/////////////////////////////////////////////////////////////
-  keys[16][5].setLayer0SizePosition(KEY_LEFT_CTRL,1,0, 0,5);
-  keys[15][5].setLayer0SizePosition(KEY_LEFT_GUI,1,0, 1,5);
-  keys[14][5].setLayer0SizePosition(KEY_LEFT_ALT,1,0, 2,5);
- 
-  keys[10][5].setLayer0SizePosition(' ',7,0, 3,5);
-  
-  keys[6][5].setLayer0SizePosition(KEY_RIGHT_CTRL,1,0, 4,5);
-  keys[5][5].setLayer0SizePosition(KEY_RIGHT_GUI,1,0, 5,5);
-  keys[4][5].setLayer0SizePosition(KEY_RIGHT_ALT,1,0, 6,5);
-  keys[3][5].setLayer0SizePosition(KEY_RIGHT_CTRL,1,0, 7,5);
-
-  keys[2][5].setLayer0Position(KEY_LEFT_ARROW, 8,5);
-  keys[1][5].setLayer0Position(KEY_DOWN_ARROW, 9,5);
-  keys[0][5].setLayer0Position(KEY_RIGHT_ARROW, 10,5);
+  keyboard->setNormalKeycap(16,5,  (byte[]){KEY_LEFT_CTRL,      '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap(15,5,  (byte[]){KEY_LEFT_GUI,       '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap(14,5,  (byte[]){KEY_LEFT_ALT,       '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap(10,5,  (byte[]){' ',                '\0','\0','\0'},       7,0,     0,0);
+  keyboard->setNormalKeycap( 6,5,  (byte[]){KEY_RIGHT_CTRL,     '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap( 5,5,  (byte[]){KEY_RIGHT_GUI,      '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap( 4,5,  (byte[]){KEY_RIGHT_ALT,      '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap( 3,5,  (byte[]){KEY_RIGHT_CTRL,     '\0','\0','\0'},       1,0,     0,0);
+  keyboard->setNormalKeycap( 2,5,  (byte[]){KEY_LEFT_ARROW,     '\0','\0','\0'},       8,5,     0,0);
+  keyboard->setNormalKeycap( 1,5,  (byte[]){KEY_DOWN_ARROW,     '\0','\0','\0'},       9,5,     0,0);
+  keyboard->setNormalKeycap( 0,5,  (byte[]){KEY_RIGHT_ARROW,    '\0','\0','\0'},      10,5,     0,0);
 }
 
 void loop() {
-  
+  keyboard->update();
 }
