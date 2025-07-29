70c62b73d07e12dabda99ee5a8f16979780bad2f
uros <karaleicuu@gmail.com>
2025-07-29 07:37:06 -0400

    dist script

--- website/dist.sh
+++ website/dist.sh
@@ -2,3 +2,8 @@ sbt server/dist
 cd client
 npm run build
 cd ..
+unzip server/target/universal/server-0.5.zip
+rm -rf dist
+mv server-0.5 dist
+cp -r server/public dist/public
+cp -r client/dist dist/public/dist
