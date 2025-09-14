e34523196a208a8b4b754d56dc1389eaf98043a9
Uros Karaleic <karaleicuu@gmail.com>
2025-09-14 23:28:25 +0200

    kinda finished keyboard builder

--- arduino/systemvi_keyboard_library_platformio/lib/Shared/src/builders/KeyboardBuilder.h
+++ arduino/systemvi_keyboard_library_platformio/lib/Shared/src/builders/KeyboardBuilder.h
@@ -1,24 +1,58 @@
 #ifndef KEYBOARDBUILDER_H
 #define KEYBOARDBUILDER_H
+#include "keyboard/SystemVIKeyboard.h"
 
 class KeyboardBuilder {
-private:
     int *columnPins,*rowPins;
     int columns,rows,reportedColumns,reportedRows;
     bool debugPrint;
     char *name;
 public:
     KeyboardBuilder() {
-        columnPins=nullptr;
-        rowPins=nullptr;
-        columns=-1;
-        rows=-1;
-        reportedColumns=-1;
-        reportedRows=-1;
-        debugPrint = false;
-        name=nullptr;
+        this->columnPins = nullptr;
+        this->rowPins = nullptr;
+        this->columns = -1;
+        this->rows = -1;
+        this->reportedColumns = -1;
+        this->reportedRows = -1;
+        this->debugPrint = false;
+        this->name = nullptr;
     }
-    
+    KeyboardBuilder* setColumns(int columns, int *columnPins) {
+        this->columns = columns;
+        this->reportedColumns = columns;
+        this->columnPins = columnPins;
+    }
+    KeyboardBuilder* setRows(int rows, int *rowPins) {
+        this->rows = rows;
+        this->reportedRows = rows;
+        this->rowPins = rowPins;
+    }
+    KeyboardBuilder* setReportedColumns(int reportedColumns) {
+        this->reportedColumns=reportedColumns;
+    }
+    KeyboardBuilder* setReportedRows(int reportedRows) {
+        this->reportedRows=reportedRows;
+    }
+    KeyboardBuilder* setDebugPrint(bool debugPrint) {
+        this->debugPrint=debugPrint;
+    }
+    KeyboardBuilder* setName(char *name) {
+        this->name = name;
+    }
+
+    SystemVIKeyboard* build() {
+        return new SystemVIKeyboard(
+            this->name,
+            this->columns,
+            this->rows,
+            this->columnPins,
+            this->rowPins,
+            this->debugPrint,
+            this->reportedColumns,
+            this->reportedRows
+        );
+    }
 };
 
 #endif //KEYBOARDBUILDER_H
--- arduino/systemvi_keyboard_library_platformio/src/dactyl/left/main.cpp
+++ arduino/systemvi_keyboard_library_platformio/src/dactyl/left/main.cpp
@@ -1,11 +1,27 @@
 #include "Arduino.h"
 #include "Keyboard.h"
-#include "../../../lib/Shared/src/keyboard/SystemVIKeyboard.h"
+#include "builders/KeyboardBuilder.h"
+#include "keyboard/SystemVIKeyboard.h"
+
+SystemVIKeyboard *keyboard;
+char name[]="dactyl 7x5";
+int columns = 7;
+int rows = 6;
+int columnPins[] = {0,1,2,3,6,7,8};
+int rowPins[] = {9,10,11,12,15,14};
 
 void setup() {
-
+    keyboard = KeyboardBuilder()
+        .setName(name)
+        ->setColumns(columns,columnPins)
+        ->setRows(rows,rowPins)
+        ->setDebugPrint(true)
+        ->build();
+    //
+    keyboard->setNormalKeycap(0,0, (char[]){'\0','\0','\0','\0',},0,0,0,0,0,0);
 }
 
 void loop() {
 
+    keyboard->update();
 }
