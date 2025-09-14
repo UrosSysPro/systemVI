1d42e3cc272160ae82ab2ee0f6f88f51814d084b
Uros Karaleic <karaleicuu@gmail.com>
2025-09-14 20:41:38 +0200

    start on keyboard builder

--- arduino/systemvi_keyboard_library_platformio/lib/Shared/src/builders/KeyboardBuilder.h
+++ arduino/systemvi_keyboard_library_platformio/lib/Shared/src/builders/KeyboardBuilder.h
@@ -3,9 +3,22 @@
 
 class KeyboardBuilder {
 private:
-
+    int *columnPins,*rowPins;
+    int columns,rows,reportedColumns,reportedRows;
+    bool debugPrint;
+    char *name;
 public:
-
+    KeyboardBuilder() {
+        columnPins=nullptr;
+        rowPins=nullptr;
+        columns=-1;
+        rows=-1;
+        reportedColumns=-1;
+        reportedRows=-1;
+        debugPrint = false;
+        name=nullptr;
+    }
+    
 };
 
 #endif //KEYBOARDBUILDER_H
