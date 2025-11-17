8a0867616097b72b31caf542cc40e0681b60ca62
Uros Karaleic <karaleicuu@gmail.com>
2025-11-17 21:46:52 +0100

    neopixel k_pro_blue

---
+++ arduino/systemvi_keyboard_library_platformio/src/k_pro_blue/main.cpp
@@ -0,0 +1,28 @@
+#include <Adafruit_NeoPixel.h>
+//#ifdef __AVR__
+//  #include <avr/power.h>
+//#endif
+#define PIN       28
+#define NUMPIXELS 61
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
+
+void setup() {
+// #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+//     clock_prescale_set(clock_div_1);
+// #endif
+
+    pixels.begin();
+}
+
+void loop() {
+    pixels.clear();
+
+    for(int i=0; i<NUMPIXELS; i++) {
+
+        pixels.setPixelColor(i, pixels.Color(200, 0, 0));
+        pixels.show();
+        delay(DELAYVAL);
+    }
+}
