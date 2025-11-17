c972c7163b0928746cfebb5d623e91df092b6a66
Uros Karaleic <karaleicuu@gmail.com>
2025-11-17 23:50:14 +0100

    rainbow example keyboard

--- arduino/systemvi_keyboard_library_platformio/src/k_pro_blue/main.cpp
+++ arduino/systemvi_keyboard_library_platformio/src/k_pro_blue/main.cpp
@@ -1,21 +1,53 @@
 #include <Adafruit_NeoPixel.h>
+#include "../../lib/Shared/src/keyboard/SystemVIKeyboard.h"
 
 #define PIN       28
 #define NUMPIXELS 61
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 500
+Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 20
 
+int rows=5;
+int rowPins[] = {0, 2, 4, 5, 14};
+
+int columns = 14;
+int columnPins[] = { 12, 11, 10, 9, 8, 7, 6, 3, 1, 27, 13, 15, 26, 29 };
+
+char name[] = "k_pro_blue";
+#define NUM_LEDS 61    // Number of LEDs in your strip
+SystemVIKeyboard *keyboard;
+
+// Convert color wheel value (0–255) into RGB
+uint32_t wheel(byte pos) {
+    pos = 255 - pos;
+    if (pos < 85) {
+        return strip.Color(255 - pos * 3, 0, pos * 3);
+    }
+    if (pos < 170) {
+        pos -= 85;
+        return strip.Color(0, pos * 3, 255 - pos * 3);
+    }
+    pos -= 170;
+    return strip.Color(pos * 3, 255 - pos * 3, 0);
+}
+
+void rainbowCycle(uint8_t wait) {
+    static uint16_t j = 0;
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        int color_index = (i * 256 / NUM_LEDS + j) & 255;
+        strip.setPixelColor(i, wheel(color_index));
+    }
+
+    strip.show();
+    j++;        // slowly move rainbow forward
+    delay(wait);
+}
 void setup() {
-    pixels.begin();
+    strip.begin();
+    strip.show();
 }
 
 void loop() {
-    pixels.clear();
-
-    for(int i=0; i<NUMPIXELS; i++) {
-
-        pixels.setPixelColor(i, pixels.Color(0, 0, 20,0));
-        pixels.show();
-        delay(DELAYVAL);
-    }
+    rainbowCycle(20); // smaller number = faster animation
 }
+
