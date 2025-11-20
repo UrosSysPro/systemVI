d356d92877568e636f9636cac47267ff740452ea
Uros Karaleic <karaleicuu@gmail.com>
2025-11-20 15:25:07 +0100

    doobie test

--- website/tests/sqlite/src/main/scala/net/systemvi/tests/sqlite/Main.scala
+++ website/tests/sqlite/src/main/scala/net/systemvi/tests/sqlite/Main.scala
@@ -4,9 +4,12 @@ import cats.effect.*
 import cats.*
 import cats.effect.implicits.*
 import cats.implicits.*
+import doobie.*
+import doobie.implicits.*
 
 object Main extends IOApp.Simple {
   override def run: IO[Unit] = for{
     _ <- IO.print("hello from sqlite test")
+//    ta <-
   }yield()
 }
