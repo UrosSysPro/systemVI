928bc688ba8a27855b123984d676fa0879be88f9
Uros Karaleic <karaleicuu@gmail.com>
2026-01-23 00:16:11 +0100

    request with cors issue

--- website/client/src/main/scala/net/systemvi/website/views/KeyboardsPageView.scala
+++ website/client/src/main/scala/net/systemvi/website/views/KeyboardsPageView.scala
@@ -7,21 +7,30 @@ import net.systemvi.website.darkproject.big_title.BigTitle
 import net.systemvi.website.darkproject.footer.Footer
 import net.systemvi.website.darkproject.navbar.Navbar
 import net.systemvi.website.darkproject.section.{Section, SectionItem}
-import net.systemvi.website.darkproject.slider.ImageSlider
 import org.scalajs.dom
 
 def KeyboardsPageView():HtmlElement = {
-  val keyboards=KeyboardApi.all()
+//  val keyboards=KeyboardApi.all()
+  val response = dom.fetch("http://localhost:8080/api/keyboards",new {
+    val `Allow-Cross-Origin` = true
+  })
+
+  response.`then`{ response=>
+    response.json().`then`{json=>
+      dom.console.log(json)
+    }
+  }
+
   div(
     cls:="flex flex-col items-center pt-24",
     div(
       className:="flex flex-col justify-start w-full max-w-[1450px]",
       Navbar(),
       BigTitle("Keyboards"),
-      Section(
-        title = "",
-        items = keyboards.map(k=>SectionItem(k.name,k.images.head,KeyboardPage(k.id)))
-      ),
+//      Section(
+//        title = "",
+//        items = keyboards.map(k=>SectionItem(k.name,k.images.head,KeyboardPage(k.id)))
+//      ),
       Footer(),
     )
   )
