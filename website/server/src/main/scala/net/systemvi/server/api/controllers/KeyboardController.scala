9c1f2c950064c4c46de2c2e732f26057cea3cf05
Uros Karaleic <karaleicuu@gmail.com>
2026-01-25 17:25:44 +0100

    simpler keyboard api

--- website/server/src/main/scala/net/systemvi/server/api/controllers/KeyboardController.scala
+++ website/server/src/main/scala/net/systemvi/server/api/controllers/KeyboardController.scala
@@ -17,124 +17,75 @@ import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import org.http4s.circe.CirceEntityDecoder.circeEntityDecoder
 import org.http4s.circe.CirceSensitiveDataEntityDecoder.circeEntityDecoder
 import org.http4s.dsl.io.*
+import java.util.UUID
+import scala.util.*
+
+def getKeyboardDto(uuid: UUID)(using context: AppContext[IO]): IO[KeyboardDto] =
+  for{
+    keyboard <- context.db.keyboards.get(uuid).map(_.getOrElse(throw Exception("keyboard not found")))
+    switchOption <- context.db.switches.get(keyboard.switchUUID)
+    switch = switchOption.get
+
+    switchManufacturerOption <- context.db.manufacturers.get(switch.manufacturerUUID)
+    switchManufacturer = switchManufacturerOption.get
+
+    images <- context.db.entityImages.get(keyboard.uuid)
+    imageDtos = images.map(a=>EntityImageDto(a.imageUrl,a.order))
+
+    specs <- context.db.entitySpecifications.get(keyboard.uuid)
+    specDtos = specs.map(a=>EntitySpecificationDto(a.key,a.value,a.order))
+
+    profile = KeyboardProfile.values.find(_.id == keyboard.profileId).get
+
+    filament <- context.db.filaments.get(keyboard.filamentUUID).map(_.get)
+    filamentManufacturer <- context.db.manufacturers.get(filament.manufacturerUUID).map(_.get)
+    polymer = FilamentPolymer.values.find(_.id == filament.polymerId).get
+
+    switchType = SwitchType.values.find(_.id == switch.switchTypeId).get
+  }yield KeyboardDto(
+    uuid = keyboard.uuid,
+    name = keyboard.name,
+    codeName = keyboard.codeName,
+    profile = KeyboardProfileDto(
+      id = profile.id,
+      name = profile.name,
+    ),
+    switch = SwitchDto(
+      uuid = switch.uuid,
+      name = switch.name,
+      switchType = SwitchTypeDto(
+        id = switchType.id,
+        name = switchType.name,
+      ),
+      manufacturer = ManufacturerDto(
+        uuid = switchManufacturer.uuid,
+        name = switchManufacturer.name,
+      ),
+    ),
+    filament = FilamentDto(
+      polymer = FilamentPolymerDto(
+        name = polymer.name
+      ),
+      manufacturer = ManufacturerDto(
+        uuid = filamentManufacturer.uuid,
+        name = filamentManufacturer.name,
+      ),
+      name = filament.name,
+    ),
+    images = imageDtos,
+    specs = specDtos,
+  )
+
 
 def keyboardController(using context: AppContext[IO]) = HttpRoutes.of[IO]{
   case GET -> Root => for{
     keyboards <- context.db.keyboards.get()
-    dtos <- keyboards.map{keyboard=>
-      for{
-        switchOption <- context.db.switches.get(keyboard.switchUUID)
-        switch = switchOption.get
-
-        switchManufacturerOption <- context.db.manufacturers.get(switch.manufacturerUUID)
-        switchManufacturer = switchManufacturerOption.get
-
-        images <- context.db.entityImages.get(keyboard.uuid)
-        imageDtos = images.map(a=>EntityImageDto(a.imageUrl,a.order))
-
-        specs <- context.db.entitySpecifications.get(keyboard.uuid)
-        specDtos = specs.map(a=>EntitySpecificationDto(a.key,a.value,a.order))
-
-        filament <- context.db.filaments.get(keyboard.filamentUUID).map(_.get)
-        filamentManufacturer <- context.db.manufacturers.get(filament.manufacturerUUID).map(_.get)
-        polymer = FilamentPolymer.values.find{_.id==filament.polymerId}.get
-
-        profile = KeyboardProfile.values.find(_.id == keyboard.profileId).get
-        switchType = SwitchType.values.find(_.id == switch.switchTypeId).get
-      }yield KeyboardDto(
-        uuid = keyboard.uuid,
-        name = keyboard.name,
-        codeName = keyboard.codeName,
-        profile = KeyboardProfileDto(
-          id = profile.id,
-          name = profile.name
-        ),
-        switch = SwitchDto(
-          uuid = switch.uuid,
-          name = switch.name,
-          switchType = SwitchTypeDto(
-            id = switchType.id,
-            name = switchType.name,
-          ),
-          manufacturer = ManufacturerDto(
-            uuid = switchManufacturer.uuid,
-            name = switchManufacturer.name,
-          ),
-        ),
-        filament = FilamentDto(
-          polymer = FilamentPolymerDto(
-            name = polymer.name
-          ),
-          manufacturer = ManufacturerDto(
-            uuid = filamentManufacturer.uuid,
-            name = filamentManufacturer.name,
-          ),
-          name = filament.name,
-        ),
-        images = imageDtos,
-        specs = specDtos,
-      )
-    }.sequence
+    dtos <- keyboards.map(_.uuid).traverse(getKeyboardDto)
     response <- Ok(dtos.asJson)
   } yield response
 
-  case GET -> Root / UUIDVar(id) => for{
-    keyboards <- context.db.keyboards.get(id)
-    dto <- keyboards.map{keyboard=>
-      for{
-        switchOption <- context.db.switches.get(keyboard.switchUUID)
-        switch = switchOption.get
-
-        switchManufacturerOption <- context.db.manufacturers.get(switch.manufacturerUUID)
-        switchManufacturer = switchManufacturerOption.get
-
-        images <- context.db.entityImages.get(keyboard.uuid)
-        imageDtos = images.map(a=>EntityImageDto(a.imageUrl,a.order))
-
-        specs <- context.db.entitySpecifications.get(keyboard.uuid)
-        specDtos = specs.map(a=>EntitySpecificationDto(a.key,a.value,a.order))
-
-        profile = KeyboardProfile.values.find(_.id == keyboard.profileId).get
-
-        filament <- context.db.filaments.get(keyboard.filamentUUID).map(_.get)
-        filamentManufacturer <- context.db.manufacturers.get(filament.manufacturerUUID).map(_.get)
-        polymer = FilamentPolymer.values.find(_.id == filament.polymerId).get
-
-        switchType = SwitchType.values.find(_.id == switch.switchTypeId).get
-      }yield KeyboardDto(
-        uuid = keyboard.uuid,
-        name = keyboard.name,
-        codeName = keyboard.codeName,
-        profile = KeyboardProfileDto(
-          id = profile.id,
-          name = profile.name,
-        ),
-        switch = SwitchDto(
-          uuid = switch.uuid,
-          name = switch.name,
-          switchType = SwitchTypeDto(
-            id = switchType.id,
-            name = switchType.name,
-          ),
-          manufacturer = ManufacturerDto(
-            uuid = switchManufacturer.uuid,
-            name = switchManufacturer.name,
-          ),
-        ),
-        filament = FilamentDto(
-          polymer = FilamentPolymerDto(
-            name = polymer.name
-          ),
-          manufacturer = ManufacturerDto(
-            uuid = filamentManufacturer.uuid,
-            name = filamentManufacturer.name,
-          ),
-          name = filament.name,
-        ),
-        images = imageDtos,
-        specs = specDtos,
-      )
-    }.sequence
+  case GET -> Root / UUIDVar(uuid) => for{
+    dto <- getKeyboardDto(uuid)
     response <- Ok(dto.asJson)
   } yield response
 }
