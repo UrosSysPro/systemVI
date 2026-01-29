57ccea28559a8e429c70f8ebc947a4643c594026
Uros Karaleic <karaleicuu@gmail.com>
2026-01-29 19:25:57 +0100

    random

--- arduino/systemvi_keyboard_library_platformio/src/receiver/main.cpp
+++ arduino/systemvi_keyboard_library_platformio/src/receiver/main.cpp
@@ -2,13 +2,10 @@
 #include <esp_now.h>
 #include <esp_wifi.h>
 #include <Arduino.h>
-#include "Adafruit_TinyUSB.h"
+#include "USB.h"
+#include "USBHIDKeyboard.h"
 
-Adafruit_USBD_HID usb_hid;
-
-uint8_t const desc_hid_report[] = {
-    TUD_HID_REPORT_DESC_KEYBOARD()
-};
+USBHIDKeyboard Keyboard;
 
 volatile char justClicked;
 
@@ -16,10 +13,6 @@ void onReceive(const uint8_t *senderAddress, const uint8_t *data, int len) {
     if (len == 1) {
         justClicked = data[0];
     }
-}
-
-void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
-
 }
 
 void setup() {
@@ -41,45 +34,20 @@ void setup() {
     }
 
     esp_now_register_recv_cb(onReceive);
-
-    if (!TinyUSBDevice.isInitialized()) {
-        TinyUSBDevice.begin(0);
-    }
-    usb_hid.setBootProtocol(HID_ITF_PROTOCOL_KEYBOARD);
-    usb_hid.setPollInterval(2);
-    usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
-    usb_hid.setStringDescriptor("TinyUSB Keyboard");
-    usb_hid.setReportCallback(nullptr,hid_report_callback);
-    usb_hid.begin();
-    // if (TinyUSBDevice.mounted()){
-    //     TinyUSBDevice.detach();
-    //     delay(10);
-    //     TinyUSBDevice.attach();
-    // }
+    USB.begin();
+    Keyboard.begin();
 }
 
 
 void loop() {
     delay(10);
-#ifdef TINYUSB_NEED_POLLING_TASK
-    TinyUSBDevice.task();
-#endif
 
     if (justClicked) {
-        if (TinyUSBDevice.mounted()&&usb_hid.ready()) {
-            uint8_t keycode[6] = { HID_KEY_A, 0, 0, 0, 0, 0 };
-            usb_hid.keyboardReport(0, 0, keycode);
-            delay(100);
-            usb_hid.keyboardRelease(0);
-        }else {
-            Serial.print("TinyUSBDevice.mounted(): ");
-            Serial.println(TinyUSBDevice.mounted());
-            Serial.print("usb_hid.ready(): ");
-            Serial.println(usb_hid.ready());
-        }
-
         Serial.print("Just clicked: ");
         Serial.println(justClicked);
+
+        Keyboard.write(justClicked);
+
         justClicked = 0;
     }
 }
