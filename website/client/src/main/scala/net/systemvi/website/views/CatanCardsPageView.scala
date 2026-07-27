8afabd6be415da5f03edaa48910aaa5ca763bd91
Uros Karaleic <karaleicuu@gmail.com>
2026-07-27 19:50:33 +0200

    catan cards

--- website/client/src/main/scala/net/systemvi/website/views/CatanCardsPageView.scala
+++ website/client/src/main/scala/net/systemvi/website/views/CatanCardsPageView.scala
@@ -3,15 +3,39 @@ package net.systemvi.website.views
 import com.raquo.laminar.api.L.{*, given}
 import org.scalajs.dom.*
 
-case class Card(icon: String, background: String)
+case class Card(icon: String, background: String, color: String, name: String)
 
 private def CardComponent(card: Card): HtmlElement = {
+
+  def title(mods: Modifier[HtmlElement]*)={
+    div(
+      height.rem(4),
+      background(card.color),
+      display.flex, alignItems.center,
+      paddingLeft.rem(1),
+      fontWeight("bold"),
+      color("white"),
+      fontSize.rem(1.5),
+      card.name,
+      mods
+    )
+  }
+
   div(
-    width.rem(4), height.rem(6),
+    width.rem(10), height.rem(16),
     boxShadow("0 0 10px 0px black"),
     background("white"),
     backgroundClip.borderBox,
-    padding.rem(0.25),
+    display.flex, flexDirection.column,
+    title(),
+    div(
+      flex("1"),
+      display.flex, justifyContent.center, alignItems.center,
+      img(
+        src(card.icon)
+      )
+    ),
+    title(transform("rotate(180deg)")),
   )
 }
 
@@ -23,17 +47,45 @@ def CatanCardsPageView: HtmlElement = {
     "public/images/catan/stone100x100.png",
     "public/images/catan/wheat100x100.png",
     "public/images/catan/wood96x96.png",
+    "public/images/catan/knight.png",
+    "public/images/catan/plusOne.png",
+    "public/images/catan/collect.png",
   )
 
   val backgrounds = List.fill(icons.length){""}
 
-  val cards = icons
-    .zip(backgrounds)
-    .map(Card.apply)
+  val colors = List(
+    "#f67441",
+    "green",
+    "#75849d",
+    "#f9b621",
+    "darkgreen",
+    "#c4939c",
+    "#a2a2cc",
+    "#222",
+  )
+  val names = List(
+    "Brick",
+    "Sheep",
+    "Stone",
+    "Wheat",
+    "Wood",
+    "Knight",
+    "+1 Point",
+    "Collect All",
+  )
 
+  val cards: List[Card] = for(i<-icons.indices.toList)
+    yield Card(
+      icons(i),
+      backgrounds(i),
+      colors(i),
+      names(i),
+    )
 
   div(
     display.flex, justifyContent.center, alignItems.center, gap.rem(3),
+    height.vh(100),
     cards.map{ card =>
       CardComponent(card)
     }
